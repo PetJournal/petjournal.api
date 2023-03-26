11ede7d374ef772f6338a61e9469ad5be5d6ea7f
Dan <dansenpirdev@gmail.com>
2023-03-26 15:07:48 -0300

    test: ensure SignUpController returns 400 if no lastName is provided

--- tests/application/controllers/signup.spec.ts
+++ tests/application/controllers/signup.spec.ts
@@ -15,6 +15,23 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new Error('Missing param: firstName'))
+  })
+
+  it('Should return 400 if no lastName is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        firstName: 'any_first_name',
+        email: 'any_email@mail.com',
+        phone: 'any_phone',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        isProvicyPolicyAccepted: 'any_boolean'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: lastName'))
   })
 })
