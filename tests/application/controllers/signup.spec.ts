294374708e83c7f5cd31ef76b7a932a00c24e5b5
Dan <dansenpirdev@gmail.com>
2023-03-26 16:24:46 -0300

    test: ensure SignUpController returns 400 if isProvicyPolicyAccepted is not provided

--- tests/application/controllers/signup.spec.ts
+++ tests/application/controllers/signup.spec.ts
@@ -86,4 +86,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
+
+  it('Should return 400 if isProvicyPolicyAccepted is not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        firstName: 'any_first_name',
+        lastName: 'any_last_name',
+        email: 'any_email@mail.com',
+        phone: 'any_phone',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('isProvicyPolicyAccepted'))
+  })
 })
