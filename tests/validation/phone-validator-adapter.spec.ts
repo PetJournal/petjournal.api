3dc1c917e2afee71925bc41c44557e921496faf2
Dan <dansenpirdev@gmail.com>
2023-04-05 15:53:31 -0300

    test: ensure PhoneValidatorAdapter return true if validator returns true

--- tests/validation/phone-validator-adapter.spec.ts
+++ tests/validation/phone-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { PhoneValidatorAdapter } from '../../src/application/validation/validators'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isMobilePhone (): boolean {
+    return true
+  }
+}))
 
 const makeSut = (): PhoneValidatorAdapter => {
   return new PhoneValidatorAdapter()
@@ -7,7 +14,14 @@ const makeSut = (): PhoneValidatorAdapter => {
 describe('PhoneValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = makeSut()
+    jest.spyOn(validator, 'isMobilePhone').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_phone')
     expect(isValid).toBe(false)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_phone')
+    expect(isValid).toBe(true)
   })
 })
