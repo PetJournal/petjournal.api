c3bd58b60f75b33123ad9b747d40bcba3b678dd8
Dan <dansenpirdev@gmail.com>
2023-04-05 20:01:28 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

---
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from '../../../src/infra/criptography/bcrypt-adapter'
+
+describe('Bcrypt Adapter', () => {
+  it('Should call bcrypt with correct values', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
