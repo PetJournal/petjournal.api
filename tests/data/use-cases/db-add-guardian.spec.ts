6961761a29b3c25c831b30ee74af6e0b094ec624
Dan <dansenpirdev@gmail.com>
2023-04-05 16:37:56 -0300

    test: ensure DbAddGuardian calls Encrypter with correct password

---
+++ tests/data/use-cases/db-add-guardian.spec.ts
@@ -0,0 +1,24 @@
+import { DbAddGuardian } from '../../../src/data/use-cases'
+
+describe('DbAddGuardian use case', () => {
+  it('Should call encrypter with correct password', async () => {
+    class EncrypterStub {
+      async encrypt (value: string): Promise<string> {
+        return await new Promise(resolve => { resolve('hashed_password') })
+      }
+    }
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddGuardian(encrypterStub)
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const guardianData = {
+      firstName: 'valid_first_name',
+      lastName: 'valid_last_name',
+      email: 'valid_email',
+      phone: 'valid_phone',
+      password: 'valid_password',
+      isProvicyPolicyAccepted: true
+    }
+    await sut.add(guardianData)
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
+})
