25deb10cf0d83fffd10326d574c19d9ada151bc8
Dan <dansenpirdev@gmail.com>
2023-04-05 14:35:21 -0300

    feat: ensure NameValidator returns true if validator returns true

--- tests/validation/name-validator-adapter.spec.ts
+++ tests/validation/name-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { NameValidatorAdapter } from '../../src/application/validation/validators'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  matches (): boolean {
+    return true
+  }
+}))
 
 const makeSut = (): NameValidatorAdapter => {
   return new NameValidatorAdapter()
@@ -7,7 +14,14 @@ const makeSut = (): NameValidatorAdapter => {
 describe('NameValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('valid_first_name', 'valid_last_name')
+    jest.spyOn(validator, 'matches').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_first_name', 'invalid_last_name')
     expect(isValid).toBe(false)
+  })
+
+  it('Sould return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_first_name', 'valid_last_name')
+    expect(isValid).toBe(true)
   })
 })
