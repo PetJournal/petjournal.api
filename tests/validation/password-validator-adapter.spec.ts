b2f5b086d652909984190095a19d81d012f89388
Dan <dansenpirdev@gmail.com>
2023-04-05 15:31:37 -0300

    test: ensure PasswordValidatorAdapter return true if validator returns true

--- tests/validation/password-validator-adapter.spec.ts
+++ tests/validation/password-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { PasswordValidatorAdapter } from '../../src/application/validation/validators/'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isStrongPassword (): boolean {
+    return true
+  }
+}))
 
 const makeSut = (): PasswordValidatorAdapter => {
   return new PasswordValidatorAdapter()
@@ -7,7 +14,14 @@ const makeSut = (): PasswordValidatorAdapter => {
 describe('PasswordValidator Adapter', () => {
   it('Should return false if validator returns false', () => {
     const sut = makeSut()
+    jest.spyOn(validator, 'isStrongPassword').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_password')
     expect(isValid).toBe(false)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_password')
+    expect(isValid).toBe(true)
   })
 })
