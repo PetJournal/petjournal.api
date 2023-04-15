62ba286b88458deeb2699f34a3490ed488df9ccc
Dan <dansenpirdev@gmail.com>
2023-04-15 17:57:35 -0300

    feat: ensure SignUp route returns an guardian account on success

---
+++ src/main/config/routes.ts
@@ -0,0 +1,7 @@
+import { type Express, Router } from 'express'
+import fg from 'fast-glob'
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+}
---
+++ src/main/routes/signup-routes.ts
@@ -0,0 +1,7 @@
+import { type Router } from 'express'
+
+export default (router: Router): void => {
+  router.post('/signup', (req, res) => {
+    res.json({ ok: 'ok' })
+  })
+}
