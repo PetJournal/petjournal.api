1289431ef7093f3b216c689df9b0a58a92b0f0c2
Dan <dansenpirdev@gmail.com>
2023-04-17 11:12:38 -0300

    feat: add SignUpController Factory

---
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -0,0 +1,17 @@
+import { EmailValidatorAdapter, NameValidatorAdapter, PasswordValidatorAdapter, PhoneValidatorAdapter } from '../../../application/validation/validators'
+import { SignUpController } from '../../../application/controllers/signup'
+import { DbAddGuardian } from '../../../data/use-cases/db-add-guardian'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { GuardianAccountRepository } from '../../../infra/repos/postgresql/guardian-account-repository'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const guardianAccountRepository = new GuardianAccountRepository()
+  const dbAddGuardian = new DbAddGuardian(guardianAccountRepository, bcryptAdapter)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const nameValidatorAdapter = new NameValidatorAdapter()
+  const passwordValidatorAdapter = new PasswordValidatorAdapter()
+  const phoneValidatorAdapter = new PhoneValidatorAdapter()
+  return new SignUpController(dbAddGuardian, emailValidatorAdapter, nameValidatorAdapter, passwordValidatorAdapter, phoneValidatorAdapter)
+}
