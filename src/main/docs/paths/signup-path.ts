a657670492f037d1e3f4a2598153466840e47165
Dan <dansenpirdev@gmail.com>
2023-04-17 15:35:01 -0300

    docs: add docs paths

---
+++ src/main/docs/paths/signup-path.ts
@@ -0,0 +1,35 @@
+export const signUpPath = {
+  post: {
+    tags: ['guardian'],
+    summary: 'adds a new guardian',
+    description: '',
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/signUpParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'boolean'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
