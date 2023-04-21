e4994fbf9ccf615fbb8df3fa2c39570c9d6aea1a
Matheus de Gondra <matheusgondra951@gmail.com>
2023-04-20 21:43:34 -0300

    feat: ensure ForgetPasswordController returns 400 if an invalid email is provided

--- src/application/controllers/forget-password.ts
+++ src/application/controllers/forget-password.ts
@@ -1,12 +1,24 @@
-import { MissingParamError } from '../errors'
+import { InvalidParamError, MissingParamError } from '../errors'
 import { type HttpRequest, type HttpResponse, badRequest, success } from '../helpers/http'
+import { type EmailValidator } from '../validation/protocols'
 import { type Controller } from './controller'
 
 export class ForgetPasswordController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email } = httpRequest.body
     if (!email) {
       return badRequest(new MissingParamError('email'))
+    }
+
+    const isValid = this.emailValidator.isValid(email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return success('')
