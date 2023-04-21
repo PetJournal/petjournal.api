813e70109ab06718fbe6f4c04fd7e7da278cd0cc
Matheus de Gondra <matheusgondra951@gmail.com>
2023-04-21 10:59:30 -0300

    feat: ensure ForgetPasswordController return 500 if EmailValidator throws

--- src/application/controllers/forget-password.ts
+++ src/application/controllers/forget-password.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../errors'
-import { type HttpRequest, type HttpResponse, badRequest, success } from '../helpers/http'
+import { type HttpRequest, type HttpResponse, badRequest, success, serverError } from '../helpers/http'
 import { type EmailValidator } from '../validation/protocols'
 import { type Controller } from './controller'
 
@@ -11,16 +11,20 @@ export class ForgetPasswordController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email } = httpRequest.body
-    if (!email) {
-      return badRequest(new MissingParamError('email'))
-    }
+    try {
+      const { email } = httpRequest.body
+      if (!email) {
+        return badRequest(new MissingParamError('email'))
+      }
 
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return success('')
+      return success('')
+    } catch (error) {
+      return serverError()
+    }
   }
 }
