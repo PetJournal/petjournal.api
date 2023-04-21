3fc9a2dcfd346c275bfb049b4bbb82fcadab55b7
Matheus de Gondra <matheusgondra951@gmail.com>
2023-04-20 21:43:48 -0300

    test: ensure ForgetPasswordController returns 400 if an invalid email is provided

--- tests/application/controllers/forget-password.spec.ts
+++ tests/application/controllers/forget-password.spec.ts
@@ -1,15 +1,43 @@
 import { ForgetPasswordController } from '@/application/controllers/forget-password'
+import { type EmailValidator } from '@/application/validation/protocols'
 
-const makeSut = (): ForgetPasswordController => {
-  const sut = new ForgetPasswordController()
-  return sut
+interface SutTypes {
+  sut: ForgetPasswordController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new ForgetPasswordController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('ForgetPassword Controller', () => {
   it('Should return 400 if no email is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('Should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com'
+      }
     }
 
     const httpResponse = await sut.handle(httpRequest)
