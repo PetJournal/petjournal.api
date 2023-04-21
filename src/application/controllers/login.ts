0b254f857db7c7d6dae29ee1575b3f2e34a8b390
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-21 15:07:30 -0300

    refactor: remove code duplication

--- src/application/controllers/login.ts
+++ src/application/controllers/login.ts
@@ -1,5 +1,5 @@
 import { type Controller } from './controller'
-import { badRequest, serverError, type HttpRequest, type HttpResponse } from '../helpers/http'
+import { badRequest, serverError, type HttpRequest, type HttpResponse, success } from '../helpers/http'
 import { InvalidParamError, MissingParamError } from '../errors'
 import { type EmailValidator } from '@/application/validation/protocols'
 import { type Authentication } from '@/domain/use-cases/authentication'
@@ -15,19 +15,19 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ['email', 'password']
+      for (const field of requiredFields) {
+        if (httpRequest.body[field] === undefined) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
       const { email, password } = httpRequest.body
-      if (!email) {
-        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
-      }
-      if (!password) {
-        return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
-      }
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return await new Promise(resolve => { resolve(badRequest(new InvalidParamError('email'))) })
+        return badRequest(new InvalidParamError('email'))
       }
-      await this.authentication.auth(email, password)
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email or password'))) })
+      const authenticationModel = await this.authentication.auth(email, password)
+      return success(authenticationModel)
     } catch (error) {
       console.error(error)
       return serverError()
