147b61164fc94214009675d8a925aae6acc6d930
Dan <dansenpirdev@gmail.com>
2023-04-25 15:34:13 -0300

    test: ensure LoggerControllerDecorator calls controller with correct value

---
+++ tests/main/decorators/logger.spec.ts
@@ -0,0 +1,46 @@
+import { type Controller } from '@/application/controllers/controller'
+import { type HttpRequest, type HttpResponse } from '@/application/helpers/http'
+import { LoggerControllerDecorator } from '@/main/decorators/logger'
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: true
+      }
+      return await new Promise(resolve => { resolve(httpResponse) })
+    }
+  }
+  return new ControllerStub()
+}
+interface SutTypes {
+  controllerStub: Controller
+  sut: LoggerControllerDecorator
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LoggerControllerDecorator(controllerStub)
+  return { controllerStub, sut }
+}
+
+describe('LoggerController Decorator', () => {
+  it('Should call controller handle', async () => {
+    const { controllerStub, sut } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        firstname: 'any_first_name',
+        lastName: 'any_last_name',
+        email: 'any_email@mail.com',
+        phone: 'any_phone',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        isPrivacyPolicyAccepted: true
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenLastCalledWith(httpRequest)
+  })
+})
