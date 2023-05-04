e80a20b781403cabb62c9b52c179056db583526b
Matheus de Gondra <matheusgondra951@gmail.com>
2023-05-03 23:33:35 -0300

    test: ensure ForgetPasswordTokenGenerator call Encrypter with correct value

--- tests/data/use-cases/forget-password-token-generator.spec.ts
+++ tests/data/use-cases/forget-password-token-generator.spec.ts
@@ -3,12 +3,24 @@ import { type Encrypter } from '@/data/protocols'
 
 interface SutTypes {
   sut: ForgetPasswordTokenGenerator
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => { resolve('any_token') })
+    }
+  }
+  return new EncrypterStub()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new ForgetPasswordTokenGenerator()
+  const encrypterStub = makeEncrypter()
+  const sut = new ForgetPasswordTokenGenerator(encrypterStub)
   return {
-    sut
+    sut,
+    encrypterStub
   }
 }
 
@@ -18,5 +30,12 @@ describe('ForgetPasswordTokenGenerator', () => {
     const token = await sut.generate(1)
     expect(token).toBeTruthy()
     expect(token).toHaveLength(6)
+  })
+
+  it('Should call Encrypter with correct value', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    await sut.generate(1)
+    expect(encryptSpy).toBeCalled()
   })
 })
