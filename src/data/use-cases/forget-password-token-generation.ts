9d1d96cd21aec27972d55211670f7d1ae0ea94d0
Matheus de Gondra <matheusgondra951@gmail.com>
2023-05-03 23:33:24 -0300

    feat: ensure ForgetPasswordTokenGenerator call Encrypter with correct value

--- src/data/use-cases/forget-password-token-generation.ts
+++ src/data/use-cases/forget-password-token-generation.ts
@@ -1,7 +1,15 @@
-import { type TokenGenerator } from '../protocols'
+import { type Encrypter, type TokenGenerator } from '../protocols'
 
 export class ForgetPasswordTokenGenerator implements TokenGenerator {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
   async generate (userId: number): Promise<string> {
-    return Math.floor(100000 + Math.random() * 900000).toString()
+    const token = Math.floor(100000 + Math.random() * 900000).toString()
+    await this.encrypter.encrypt(token)
+    return token
   }
 }
