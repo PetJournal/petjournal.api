b8ca3c9c6f100ca58b57148f87ccc03f0a76953d
Matheus de Gondra <matheusgondra951@gmail.com>
2023-05-03 23:42:47 -0300

    feat: ensure ForgetPasswordTokenGenerator calls SaveTokenRepository with correct values

--- src/data/use-cases/forget-password-token-generation.ts
+++ src/data/use-cases/forget-password-token-generation.ts
@@ -1,15 +1,18 @@
-import { type Encrypter, type TokenGenerator } from '../protocols'
+import { type SaveTokenRepository, type Encrypter, type TokenGenerator } from '../protocols'
 
 export class ForgetPasswordTokenGenerator implements TokenGenerator {
   private readonly encrypter: Encrypter
+  private readonly saveTokenRepository: SaveTokenRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, saveTokenRepository: SaveTokenRepository) {
     this.encrypter = encrypter
+    this.saveTokenRepository = saveTokenRepository
   }
 
   async generate (userId: number): Promise<string> {
     const token = Math.floor(100000 + Math.random() * 900000).toString()
-    await this.encrypter.encrypt(token)
+    const encryptedToken = await this.encrypter.encrypt(token)
+    await this.saveTokenRepository.saveToken(userId, encryptedToken)
     return token
   }
 }
