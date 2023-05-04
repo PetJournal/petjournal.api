cbfe3c14282d578977b7c527bf0e1d8ab74ae7f6
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-05-04 10:29:10 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- tests/infra/criptography/bcrypt-adapter.spec.ts
+++ tests/infra/criptography/bcrypt-adapter.spec.ts
@@ -53,4 +53,11 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
+
+  it('Should return false when compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(sut, 'compare').mockReturnValueOnce(new Promise(resolve => { resolve(false) }))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
 })
