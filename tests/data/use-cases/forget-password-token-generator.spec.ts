93c7f5e05a8323b7ab58de8aab3024bb32945b68
Matheus de Gondra <matheusgondra951@gmail.com>
2023-05-03 23:26:09 -0300

    refactor: move sut creation to a factory helper method

--- tests/data/use-cases/forget-password-token-generator.spec.ts
+++ tests/data/use-cases/forget-password-token-generator.spec.ts
@@ -1,8 +1,20 @@
 import { ForgetPasswordTokenGenerator } from '@/data/use-cases/forget-password-token-generation'
+import { type Encrypter } from '@/data/protocols'
+
+interface SutTypes {
+  sut: ForgetPasswordTokenGenerator
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new ForgetPasswordTokenGenerator()
+  return {
+    sut
+  }
+}
 
 describe('ForgetPasswordTokenGenerator', () => {
   it('Should return a token on success', async () => {
-    const sut = new ForgetPasswordTokenGenerator()
+    const { sut } = makeSut()
     const token = await sut.generate(1)
     expect(token).toBeTruthy()
     expect(token).toHaveLength(6)
