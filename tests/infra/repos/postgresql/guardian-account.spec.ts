ffa58f6f31e8309d670385c658b1d0b55fc7783b
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-08 16:59:13 -0300

    test: ensure GuardianAccountRepository.loadByEmail returns a guardian when valid email is provided

--- tests/infra/repos/postgresql/guardian-account.spec.ts
+++ tests/infra/repos/postgresql/guardian-account.spec.ts
@@ -1,4 +1,5 @@
 
+import { type AddGuardianRepository } from '@/data/protocols'
 import { GuardianAccountRepository } from '@/infra/repos/postgresql/guardian-account-repository'
 import { PrismaHelper } from '@/tests/helpers/prisma-helper'
 
@@ -10,34 +11,39 @@ const makeSut = (): GuardianAccountRepository => {
   return new GuardianAccountRepository()
 }
 
+const makeFakeGuardianData = (): AddGuardianRepository.Params => ({
+  firstName: 'valid_first_name',
+  lastName: 'valid_last_name',
+  email: 'valid_email',
+  password: 'valid_password',
+  phone: 'valid_phone',
+  isPrivacyPolicyAccepted: true
+})
+
 describe('GuardianAccountRepository', () => {
   it('Should return a guardian account on success ', async () => {
     const sut = makeSut()
-    const guardianData = {
-      firstName: 'valid_first_name',
-      lastName: 'valid_last_name',
-      email: 'valid_email',
-      password: 'valid_password',
-      phone: 'valid_phone',
-      isPrivacyPolicyAccepted: true
-    }
-    const isValid = await sut.add(guardianData)
+    const isValid = await sut.add(makeFakeGuardianData())
     expect(isValid).toBe(true)
   })
 
   it('Should not return a guardian account if duplicated email or phone is provided', async () => {
     const sut = makeSut()
-    const guardianData = {
-      firstName: 'valid_first_name',
-      lastName: 'valid_last_name',
-      email: 'valid_email',
-      password: 'valid_password',
-      phone: 'valid_phone',
-      isPrivacyPolicyAccepted: true
-    }
-    const firstAttempt = await sut.add(guardianData)
-    const secondAttempt = await sut.add(guardianData)
+    const firstAttempt = await sut.add(makeFakeGuardianData())
+    const secondAttempt = await sut.add(makeFakeGuardianData())
     expect(firstAttempt).toBe(true)
     expect(secondAttempt).toBe(false)
+  })
+
+  describe('LoadAccountByEmailRepository', () => {
+    it('Should return a guardian account on success', async () => {
+      const sut = makeSut()
+      const guardianData = makeFakeGuardianData()
+      await sut.add(guardianData)
+
+      const guardian = await sut.loadByEmail(guardianData.email)
+
+      expect(guardian).toEqual({ ...guardianData, id: 1 })
+    })
   })
 })
