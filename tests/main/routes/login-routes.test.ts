c750a3fbed692fdc3d1acb7cf9dc0b7dc29bbc32
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-09 17:23:29 -0300

    test: login route integration test

---
+++ tests/main/routes/login-routes.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { PrismaHelper } from '@/tests/helpers/prisma-helper'
+
+beforeEach(() => { PrismaHelper.connect() })
+
+afterEach(async () => { await PrismaHelper.disconnect() })
+
+describe('Login Routes', () => {
+  it('Should return an access token on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@email.com',
+        password: 'Teste@123',
+        passwordConfirmation: 'Teste@123',
+        phone: '11987654321',
+        isPrivacyPolicyAccepted: true
+      })
+
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'johndoe@email.com',
+        password: 'Teste@123'
+      })
+
+    expect(response.status).toBe(200)
+    expect(response.body.accessToken).toBeTruthy()
+  })
+})
