8f173cf606fc65ebc2636a6298e8c40843849069
Samuel <samuelsoutosilva14@gmail.com>
2023-05-10 12:28:44 -0300

    test: update payload parameter to JwtPayload type and token generation to be based on ID

--- tests/infra/criptography/jwt-adapter.spec.ts
+++ tests/infra/criptography/jwt-adapter.spec.ts
@@ -3,7 +3,7 @@ import { JwtAdapter } from '@/infra/criptography/jwt-adapter'
 
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
-    return await new Promise(resolve => { resolve('any_token') })
+    return await Promise.resolve('any_token')
   }
 }))
 
@@ -15,13 +15,13 @@ describe('Jwt Adapter', () => {
   it('Should call sign with correct values', async () => {
     const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.generate('any_email@mail.com')
-    expect(signSpy).toHaveBeenCalledWith({ email: 'any_email@mail.com' }, 'secret')
+    await sut.generate({ sub: 'any_id' })
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('Should return a token on sign success', async () => {
     const sut = makeSut()
-    const accessToken = await sut.generate('any_email@mail.com')
+    const accessToken = await sut.generate({ sub: 'any_id' })
     expect(accessToken).toBe('any_token')
   })
 
@@ -30,7 +30,7 @@ describe('Jwt Adapter', () => {
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error()
     })
-    const promise = sut.generate('any_email@mail.com')
+    const promise = sut.generate({ sub: 'any_id' })
     await expect(promise).rejects.toThrow()
   })
 })
