968c1175987c901a7d0d957ca52af1a8983f6c68
Samuel <samuelsoutosilva14@gmail.com>
2023-05-12 11:46:54 -0300

    feat: add adaptMiddleware

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,21 @@
+import { type HttpRequest, type HttpResponse } from '@/application/helpers/http'
+import { type Middleware } from '@/application/middlewares/middleware'
+
+import { type Request, type Response, type NextFunction } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      header: req.headers
+    }
+    const httpResponse: HttpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
