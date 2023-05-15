3f56b90d2f1be629dcd0e9b4fc52cff6b959c34d
Samuel <samuelsoutosilva14@gmail.com>
2023-05-15 16:50:27 -0300

    refactor: add exports in protocols

---
+++ src/data/protocols/guardian/index.ts
@@ -0,0 +1,5 @@
+
+export * from './add-guardian-repository'
+export * from './load-guardian-by-email-repository'
+export * from './load-guardian-by-id-repository'
+export * from './update-access-token-repository'
