f430c5220af5272b6914e8ad3cdaff969a4843b1
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-24 10:20:00 -0300

    feat: ensure WaitingCode Controller return unauthorized if invalid forgetPasswordCode is provided

--- src/application/controllers/waiting-code.ts
+++ src/application/controllers/waiting-code.ts
@@ -1,13 +1,16 @@
 import { type Controller } from '@/application/controllers/controller'
-import { type HttpRequest, type HttpResponse, badRequest, serverError } from '@/application/helpers/http'
+import { type HttpRequest, type HttpResponse, badRequest, serverError, unauthorized } from '@/application/helpers/http'
 import { InvalidParamError, MissingParamError } from '@/application/errors'
 import { type EmailValidator } from '../validation/protocols'
+import { type ForgetCodeAuthentication } from '@/domain/use-cases'
 
 export class WaitingCodeController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly forgetCodeAuthentication: ForgetCodeAuthentication
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, forgetCodeAuthentication: ForgetCodeAuthentication) {
     this.emailValidator = emailValidator
+    this.forgetCodeAuthentication = forgetCodeAuthentication
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -17,12 +20,19 @@ export class WaitingCodeController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-
-    const { email } = httpRequest.body
+    const { email, forgetPasswordCode } = httpRequest.body
     const isEmailValid = this.emailValidator.isValid(email)
     if (!isEmailValid) {
       return badRequest(new InvalidParamError('email'))
     }
+    const codeAuthorized = await this.forgetCodeAuthentication.auth({
+      email,
+      forgetPasswordCode
+    })
+    if (!codeAuthorized) {
+      return unauthorized()
+    }
+
     return serverError(new Error())
   }
 }
---
+++ src/domain/use-cases/forget-code-authentication.ts
@@ -0,0 +1,19 @@
+import { type HashComparer, type LoadGuardianByEmailRepository } from '@/data/protocols'
+
+export interface ForgetCodeAuthentication {
+  auth: (input: ForgetCodeAuthentication.Params) => Promise<ForgetCodeAuthentication.Result>
+}
+
+export namespace ForgetCodeAuthentication {
+  export interface Params {
+    email: string
+    forgetPasswordCode: string
+  }
+
+  export type Result = boolean | null
+
+  export interface Dependencies {
+    loadGuardianByEmailRepository: LoadGuardianByEmailRepository
+    hashComparer: HashComparer
+  }
+}
