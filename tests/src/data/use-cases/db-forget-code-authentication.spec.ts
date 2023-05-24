fd1262d5adb33deb557970f6ec338475c75910b3
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-24 17:57:38 -0300

    test: ensure DbForgetCodeAuthentication return NotFoundError if not found email is provided

--- tests/src/data/use-cases/db-forget-code-authentication.spec.ts
+++ tests/src/data/use-cases/db-forget-code-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundError } from '@/application/errors'
 import { type LoadGuardianByEmailRepository } from '@/data/protocols'
 import { DbForgetCodeAuthentication } from '@/data/use-cases/db-forget-code-authentication'
 import { makeFakeGuardianWithIdData, makeLoadGuardianByEmail } from '@/tests/utils'
@@ -29,6 +30,15 @@ describe('DbForgetCodeAuthentication UseCase', () => {
       await sut.auth(fakeInput)
 
       expect(loadSpy).toHaveBeenCalledWith(fakeInput.email)
+    })
+
+    it('Should return NotFoundError if not found email is provided', async () => {
+      const { sut, loadGuardianByEmailRepositoryStub } = makeSut()
+      jest.spyOn(loadGuardianByEmailRepositoryStub, 'loadByEmail').mockResolvedValueOnce(undefined)
+
+      const result = await sut.auth(fakeInput)
+
+      expect(result).toStrictEqual(new NotFoundError('email'))
     })
   })
 })
