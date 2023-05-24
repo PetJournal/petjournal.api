66ee794462081ae50c21ad92d13c2bcef3702b14
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-24 09:21:30 -0300

    feat: ensure WaitingCode Controller return bad request if no forgetPasswordCode is provided

--- src/application/controllers/waiting-code.ts
+++ src/application/controllers/waiting-code.ts
@@ -1,9 +1,15 @@
 import { type Controller } from '@/application/controllers/controller'
-import { type HttpRequest, type HttpResponse, badRequest } from '@/application/helpers/http'
+import { type HttpRequest, type HttpResponse, badRequest, serverError } from '@/application/helpers/http'
 import { MissingParamError } from '@/application/errors'
 
 export class WaitingCodeController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new MissingParamError('email'))
+    const requiredFields = ['email', 'forgetPasswordCode']
+    for (const field of requiredFields) {
+      if (httpRequest.body[field] === undefined) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+    return serverError(new Error())
   }
 }
