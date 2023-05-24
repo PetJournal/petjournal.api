e9ed566cd547af9b892931d812b5a26a2255e208
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-24 09:38:52 -0300

    test: ensure WaitingCode Controller return bad request if invalid email is provided

--- tests/src/application/controllers/waiting-code.spec.ts
+++ tests/src/application/controllers/waiting-code.spec.ts
@@ -1,11 +1,27 @@
 import { WaitingCodeController } from '@/application/controllers/waiting-code'
-import { MissingParamError } from '@/application/errors'
+import { InvalidParamError, MissingParamError } from '@/application/errors'
 import { badRequest } from '@/application/helpers/http'
+import { type EmailValidator } from '@/application/validation/protocols'
+import { makeEmailValidator } from '@/tests/utils'
+
+interface SutTypes {
+  sut: WaitingCodeController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new WaitingCodeController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('WaitingCode Controller', () => {
   describe('validations tests', () => {
     it('should return bad request if no email is provided', async () => {
-      const sut = new WaitingCodeController()
+      const { sut } = makeSut()
       const httpRequest = {
         body: {}
       }
@@ -16,7 +32,7 @@ describe('WaitingCode Controller', () => {
     })
 
     it('should return bad request if no forgetPasswordCode is provided', async () => {
-      const sut = new WaitingCodeController()
+      const { sut } = makeSut()
       const httpRequest = {
         body: {
           email: 'valid_email'
@@ -26,6 +42,21 @@ describe('WaitingCode Controller', () => {
       const httpResponse = await sut.handle(httpRequest)
 
       expect(httpResponse).toEqual(badRequest(new MissingParamError('forgetPasswordCode')))
+    })
+
+    it('should return bad request if invalid email is provided', async () => {
+      const { sut, emailValidatorStub } = makeSut()
+      const httpRequest = {
+        body: {
+          email: 'invalid_email',
+          forgetPasswordCode: 'valid_code'
+        }
+      }
+      jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+      const httpResponse = await sut.handle(httpRequest)
+
+      expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
     })
   })
 })
