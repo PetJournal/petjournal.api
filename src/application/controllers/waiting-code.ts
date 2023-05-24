36ba6d761be85292c56a1e0bb1bb0d8b9f08dfd6
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-24 09:37:21 -0300

    feat: ensure WaitingCode Controller return bad request if invalid email is provided

--- src/application/controllers/waiting-code.ts
+++ src/application/controllers/waiting-code.ts
@@ -1,14 +1,27 @@
 import { type Controller } from '@/application/controllers/controller'
 import { type HttpRequest, type HttpResponse, badRequest, serverError } from '@/application/helpers/http'
-import { MissingParamError } from '@/application/errors'
+import { InvalidParamError, MissingParamError } from '@/application/errors'
+import { type EmailValidator } from '../validation/protocols'
 
 export class WaitingCodeController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['email', 'forgetPasswordCode']
     for (const field of requiredFields) {
       if (httpRequest.body[field] === undefined) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const { email } = httpRequest.body
+    const isEmailValid = this.emailValidator.isValid(email)
+    if (!isEmailValid) {
+      return badRequest(new InvalidParamError('email'))
     }
     return serverError(new Error())
   }
