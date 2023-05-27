69f5fb83c1f4342c94c4c456dde7970d7b6ccdb2
Samuel <samuelsoutosilva14@gmail.com>
2023-05-27 19:02:58 -0300

    feat: add change password route

---
+++ src/main/routes/change-password-routes.ts
@@ -0,0 +1,8 @@
+import { type Router } from 'express'
+import { adaptRoute } from '@/main/adapters'
+import { makeChangePasswordController } from '@/main/factories'
+import { auth } from '@/main/middlewares'
+
+export default (router: Router): void => {
+  router.post('/change-password', auth, adaptRoute(makeChangePasswordController()))
+}
