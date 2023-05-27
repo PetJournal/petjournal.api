f81bd2558c0b3c9d1a10b7ba7b8e924679897c09
Samuel <samuelsoutosilva14@gmail.com>
2023-05-26 21:25:53 -0300

    feat: standardize imports

--- src/data/use-cases/db-add-guardian.ts
+++ src/data/use-cases/db-add-guardian.ts
@@ -1,17 +1,17 @@
-import { type AddGuardianRepository, type HashGenerator } from '@/data/protocols'
 import { type AddGuardian } from '@/domain/use-cases'
+import { type AddGuardianRepository, type HashGenerator } from '@/data/protocols'
 
 export class DbAddGuardian implements AddGuardian {
   private readonly addGuardianRepository: AddGuardianRepository
-  private readonly encrypter: HashGenerator
+  private readonly hashGenerator: HashGenerator
 
-  constructor (addGuardianRepository: AddGuardianRepository, encrypter: HashGenerator) {
+  constructor ({ addGuardianRepository, hashGenerator }: AddGuardian.Dependencies) {
     this.addGuardianRepository = addGuardianRepository
-    this.encrypter = encrypter
+    this.hashGenerator = hashGenerator
   }
 
   async add (guardianData: AddGuardian.Params): Promise<AddGuardian.Result> {
-    const hashedPassword = await this.encrypter.encrypt({ value: guardianData.password })
+    const hashedPassword = await this.hashGenerator.encrypt({ value: guardianData.password })
     return await this.addGuardianRepository.add(Object.assign({}, guardianData, { password: hashedPassword }))
   }
 }
