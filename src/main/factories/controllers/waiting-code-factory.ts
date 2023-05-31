747872c7ef3a1ac9fa0db0a9192ff4e5728c2698
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-05-31 20:17:38 -0300

    feat: add Logger to SignUpController

--- src/main/factories/controllers/waiting-code-factory.ts
+++ src/main/factories/controllers/waiting-code-factory.ts
@@ -1,13 +1,16 @@
+import { type Controller } from '@/application/controllers/controller'
 import { WaitingCodeController } from '@/application/controllers/waiting-code'
 import { EmailValidatorAdapter } from '@/application/validation/validators'
 import { DbForgetCodeAuthentication } from '@/data/use-cases/db-forget-code-authentication'
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/cryptography/jwt-adapter'
 import { GuardianAccountRepository } from '@/infra/repos/postgresql/guardian-account-repository'
+import { LoggerPgRepository } from '@/infra/repos/postgresql/logger-repository'
 
 import env from '@/main/config/env'
+import { LoggerControllerDecorator } from '@/main/decorators/logger'
 
-export const makeWaitingCodeController = (): WaitingCodeController => {
+export const makeWaitingCodeController = (): Controller => {
   const salt = Number(env.salt)
   const secret = env.secret
   const emailValidator = new EmailValidatorAdapter()
@@ -21,9 +24,11 @@ export const makeWaitingCodeController = (): WaitingCodeController => {
     hashGenerator: hasher,
     tokenGenerator
   })
-
-  return new WaitingCodeController({
+  const loggerPgRepository = new LoggerPgRepository()
+  const waitingCodeController = new WaitingCodeController({
     emailValidator,
     forgetCodeAuthentication
   })
+
+  return new LoggerControllerDecorator(waitingCodeController, loggerPgRepository)
 }
