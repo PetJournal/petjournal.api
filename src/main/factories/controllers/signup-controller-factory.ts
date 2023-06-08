cc7cd780a4fc265ca0240588ebd0924589e45e70
Samuel <samuelsoutosilva14@gmail.com>
2023-06-08 19:20:50 -0300

    refactor: add sign up controller factory

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,16 +1,11 @@
+import env from '@/main/config/env'
 import { type Controller } from '@/application/protocols'
 import { SignUpController } from '@/application/controllers'
 import { DbAddGuardian } from '@/data/use-cases'
-import {
-  EmailValidatorAdapter,
-  NameValidatorAdapter,
-  PasswordValidatorAdapter,
-  PhoneValidatorAdapter
-} from '@/infra/validators'
 import { BcryptAdapter } from '@/infra/cryptography'
 import { GuardianAccountRepository, LoggerPgRepository } from '@/infra/repos/postgresql'
 import { LoggerControllerDecorator } from '@/main/decorators'
-import env from '@/main/config/env'
+import { makeSignUpValidation } from '@/main/factories'
 
 export const makeSignUpController = (): Controller => {
   const salt = Number(env.salt)
@@ -18,16 +13,11 @@ export const makeSignUpController = (): Controller => {
   const addGuardianRepository = new GuardianAccountRepository()
   const addGuardian = new DbAddGuardian({ addGuardianRepository, hashGenerator })
   const loggerPgRepository = new LoggerPgRepository()
-  const emailValidator = new EmailValidatorAdapter()
-  const nameValidator = new NameValidatorAdapter()
-  const passwordValidator = new PasswordValidatorAdapter()
-  const phoneValidator = new PhoneValidatorAdapter()
-  const signUpController = new SignUpController({
+  const validation = makeSignUpValidation()
+  const dependencies: SignUpController.Dependencies = {
     addGuardian,
-    emailValidator,
-    nameValidator,
-    passwordValidator,
-    phoneValidator
-  })
+    validation
+  }
+  const signUpController = new SignUpController(dependencies)
   return new LoggerControllerDecorator(signUpController, loggerPgRepository)
 }
