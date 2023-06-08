a3eb55ebdaa2fe79fd4e70ac4fccd9d4e6f914ec
Samuel <samuelsoutosilva14@gmail.com>
2023-06-08 16:47:54 -0300

    feat: add compare fields validation

---
+++ src/application/validation/validators/compare-fields-validation.ts
@@ -0,0 +1,15 @@
+import { type Validation } from '@/application/protocols'
+import { InvalidParamError } from '@/application/errors'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
+
+  validate (input: any): Error | void {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName)
+    }
+  }
+}
