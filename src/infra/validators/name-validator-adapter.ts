a6d2ec6a1974d12551ef9c6d1049b5328da82482
Samuel <samuelsoutosilva14@gmail.com>
2023-06-08 16:54:55 -0300

    refactor: name validator adapter now receives a parameter

--- src/infra/validators/name-validator-adapter.ts
+++ src/infra/validators/name-validator-adapter.ts
@@ -2,10 +2,10 @@ import { type NameValidator } from '@/application/validation'
 import validator from 'validator'
 
 export class NameValidatorAdapter implements NameValidator {
-  isValid (firstName: string, lastName: string): boolean {
+  isValid (name: string): boolean {
     const isValidName = (field: string): boolean => validator.matches(field, /^[a-zA-Z]{3,}$/)
 
-    if (isValidName(firstName) && isValidName(lastName)) {
+    if (isValidName(name)) {
       return true
     }
 
