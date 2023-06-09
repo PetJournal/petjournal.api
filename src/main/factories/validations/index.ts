5ec38954901ed2d2d59079ca152e11a46eaaeaa9
Samuel <samuelsoutosilva14@gmail.com>
2023-06-09 19:38:40 -0300

    feat: add change password validation factory

--- src/main/factories/validations/index.ts
+++ src/main/factories/validations/index.ts
@@ -1,3 +1,4 @@
 export * from './login-validation-factory'
 export * from './signup-validation-factory'
 export * from './forget-password-validation-factory'
+export * from './change-password-validation-factory'
