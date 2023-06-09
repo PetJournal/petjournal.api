5a8916ef5c24c3f01286376a21106688acdf2773
Samuel <samuelsoutosilva14@gmail.com>
2023-06-09 19:39:15 -0300

    refactor: add validation in change password controller

--- src/main/factories/controllers/change-password-factory.ts
+++ src/main/factories/controllers/change-password-factory.ts
@@ -1,11 +1,11 @@
+import env from '@/main/config/env'
 import { type Controller } from '@/application/protocols'
 import { ChangePasswordController } from '@/application/controllers'
-import { DbChangePassword } from '@/data/use-cases'
 import { BcryptAdapter } from '@/infra/cryptography'
-import { PasswordValidatorAdapter } from '@/infra/validators'
 import { GuardianAccountRepository, LoggerPgRepository } from '@/infra/repos/postgresql'
+import { DbChangePassword } from '@/data/use-cases'
 import { LoggerControllerDecorator } from '@/main/decorators'
-import env from '@/main/config/env'
+import { makeChangePasswordValidation } from '@/main/factories'
 
 export const makeChangePasswordController = (): Controller => {
   const salt = Number(env.salt)
@@ -14,7 +14,11 @@ export const makeChangePasswordController = (): Controller => {
   const hashGenerator = new BcryptAdapter(salt)
   const changePassword = new DbChangePassword({ hashGenerator, loadGuardianByIdRepository, updateGuardianPasswordRepository })
   const loggerPgRepository = new LoggerPgRepository()
-  const passwordValidator = new PasswordValidatorAdapter()
-  const changePasswordController = new ChangePasswordController({ changePassword, passwordValidator })
+  const validation = makeChangePasswordValidation()
+  const dependencies: ChangePasswordController.Dependencies = {
+    changePassword,
+    validation
+  }
+  const changePasswordController = new ChangePasswordController(dependencies)
   return new LoggerControllerDecorator(changePasswordController, loggerPgRepository)
 }
