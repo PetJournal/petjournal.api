6110c48a3028848991c45798ee37fe4ca8c65fab
Samuel <samuelsoutosilva14@gmail.com>
2023-06-10 15:05:45 -0300

    refactor: remove dependencies from use case and add to Authentication factory

--- src/main/factories/controllers/login-controller-factory.ts
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -1,27 +1,17 @@
-import env from '@/main/config/env'
 import { type Controller } from '@/application/protocols'
 import { LoginController } from '@/application/controllers'
-import { BcryptAdapter, JwtAdapter } from '@/infra/cryptography'
-import { GuardianAccountRepository, LoggerPgRepository } from '@/infra/repos/postgresql'
+import { LoggerPgRepository } from '@/infra/repos/postgresql'
 import { LoggerControllerDecorator } from '@/main/decorators'
-import { makeLoginValidation } from '@/main/factories'
-import { DbAuthentication } from '@/data/use-cases'
+import { makeLoginValidation, makeDbAuthentication } from '@/main/factories'
 
 export const makeLoginController = (): Controller => {
-  const salt = Number(env.salt)
-  const secret = env.secret
-  const hashGenerator = new BcryptAdapter(salt)
-  const hashComparer = new BcryptAdapter(salt)
-  const tokenGenerator = new JwtAdapter(secret)
-  const loadGuardianByEmailRepository = new GuardianAccountRepository()
-  const updateAccessTokenRepository = new GuardianAccountRepository()
-  const authentication = new DbAuthentication({ loadGuardianByEmailRepository, hashGenerator, hashComparer, tokenGenerator, updateAccessTokenRepository })
-  const loggerPgRepository = new LoggerPgRepository()
+  const authentication = makeDbAuthentication()
   const validation = makeLoginValidation()
   const dependencies: LoginController.Dependencies = {
-    validation,
-    authentication
+    authentication,
+    validation
   }
   const loginController = new LoginController(dependencies)
+  const loggerPgRepository = new LoggerPgRepository()
   return new LoggerControllerDecorator(loginController, loggerPgRepository)
 }
