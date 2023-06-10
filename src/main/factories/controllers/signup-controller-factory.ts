f0565608cae069fc00c07dd03262c5c9a2035e31
Samuel <samuelsoutosilva14@gmail.com>
2023-06-10 14:55:29 -0300

    refactor: remove dependencies from use case and add to addGuardian factory

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,23 +1,17 @@
-import env from '@/main/config/env'
 import { type Controller } from '@/application/protocols'
 import { SignUpController } from '@/application/controllers'
-import { DbAddGuardian } from '@/data/use-cases'
-import { BcryptAdapter } from '@/infra/cryptography'
-import { GuardianAccountRepository, LoggerPgRepository } from '@/infra/repos/postgresql'
+import { LoggerPgRepository } from '@/infra/repos/postgresql'
 import { LoggerControllerDecorator } from '@/main/decorators'
-import { makeSignUpValidation } from '@/main/factories'
+import { makeSignUpValidation, makeDbAddGuardian } from '@/main/factories'
 
 export const makeSignUpController = (): Controller => {
-  const salt = Number(env.salt)
-  const hashGenerator = new BcryptAdapter(salt)
-  const addGuardianRepository = new GuardianAccountRepository()
-  const addGuardian = new DbAddGuardian({ addGuardianRepository, hashGenerator })
-  const loggerPgRepository = new LoggerPgRepository()
+  const addGuardian = makeDbAddGuardian()
   const validation = makeSignUpValidation()
   const dependencies: SignUpController.Dependencies = {
     addGuardian,
     validation
   }
   const signUpController = new SignUpController(dependencies)
+  const loggerPgRepository = new LoggerPgRepository()
   return new LoggerControllerDecorator(signUpController, loggerPgRepository)
 }
