d619b53636e885819be8e9e39401beda42e81f6b
Samuel <samuelsoutosilva14@gmail.com>
2023-06-10 15:21:19 -0300

    refactor: remove dependencies from use case and add to ForgetPassword factory

--- src/main/factories/controllers/forget-password-factory.ts
+++ src/main/factories/controllers/forget-password-factory.ts
@@ -1,34 +1,17 @@
-import env from '@/main/config/env'
 import { type Controller } from '@/application/protocols'
 import { ForgetPasswordController } from '@/application/controllers'
-import { DbForgetPassword, ForgetPasswordTokenGenerator } from '@/data/use-cases'
-import { BcryptAdapter } from '@/infra/cryptography'
-import { NodeMailerAdapter } from '@/infra/communication'
-import { GuardianAccountRepository, LoggerPgRepository } from '@/infra/repos/postgresql'
 import { LoggerControllerDecorator } from '@/main/decorators'
-import { makeForgetPasswordValidation } from '@/main/factories'
+import { LoggerPgRepository } from '@/infra/repos/postgresql'
+import { makeForgetPasswordValidation, makeDbForgetPassword } from '@/main/factories'
 
 export const makeForgetPasswordController = (): Controller => {
-  const salt = Number(env.salt)
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const saveTokenRepository = new GuardianAccountRepository()
-  const loadGuardianByEmailRepository = new GuardianAccountRepository()
-  const tokenGenerator = new ForgetPasswordTokenGenerator(bcryptAdapter, saveTokenRepository)
-  const transporter = {
-    service: 'gmail',
-    auth: {
-      user: env.mailUser,
-      pass: env.mailPass
-    }
-  }
-  const nodeMailerAdapter = new NodeMailerAdapter(transporter)
-  const forgetPassword = new DbForgetPassword({ loadGuardianByEmailRepository, tokenGenerator, emailService: nodeMailerAdapter })
-  const loggerPgRepository = new LoggerPgRepository()
+  const forgetPassword = makeDbForgetPassword()
   const validation = makeForgetPasswordValidation()
   const dependencies: ForgetPasswordController.Dependencies = {
     validation,
     forgetPassword
   }
   const forgetPasswordController = new ForgetPasswordController(dependencies)
+  const loggerPgRepository = new LoggerPgRepository()
   return new LoggerControllerDecorator(forgetPasswordController, loggerPgRepository)
 }
