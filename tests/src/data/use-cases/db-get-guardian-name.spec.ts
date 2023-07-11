7dbaefe4864ee29aa7a758b0089c73c69f3e1bae
Matheus de Gondra <matheusgondra951@gmail.com>
2023-07-10 22:44:58 -0300

    refactor: move sut creation to factory method

--- tests/src/data/use-cases/db-get-guardian-name.spec.ts
+++ tests/src/data/use-cases/db-get-guardian-name.spec.ts
@@ -1,11 +1,25 @@
+import { type LoadGuardianByIdRepository } from '@/data/protocols'
 import { DbGetGuardianName } from '@/data/use-cases'
 import { makeFakeGuardianRepository } from '@/tests/utils'
+
+interface SutTypes {
+  sut: DbGetGuardianName
+  guardianRepositoryStub: LoadGuardianByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const guardianRepositoryStub = makeFakeGuardianRepository()
+  const sut = new DbGetGuardianName({ guardianRepository: guardianRepositoryStub })
+  return {
+    sut,
+    guardianRepositoryStub
+  }
+}
 
 describe('DbGetGuardianName', () => {
   describe('GuardianRepository', () => {
     it('Should call loadById method with correct value', async () => {
-      const guardianRepositoryStub = makeFakeGuardianRepository()
-      const sut = new DbGetGuardianName({ guardianRepository: guardianRepositoryStub })
+      const { sut, guardianRepositoryStub } = makeSut()
       const loadByIdSpy = jest.spyOn(guardianRepositoryStub, 'loadById')
       await sut.load('any_id')
       expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
