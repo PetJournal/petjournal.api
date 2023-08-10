4273691f0d6749a87672e916a7dbdcfc20c8df3d
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-08-10 08:50:38 -0300

    feat: ensure DbAddPet call loadById with correct values

--- src/data/use-cases/pet/db-add-pet.ts
+++ src/data/use-cases/pet/db-add-pet.ts
@@ -1,11 +1,28 @@
 import { NotFoundError } from '@/application/errors'
+import { type LoadGuardianByIdRepository } from '@/data/protocols'
 import { type AddPet } from '@/domain/use-cases'
 
 export class DbAddPet implements AddPet {
+  private readonly guardianRepository: LoadGuardianByIdRepository
+
+  constructor ({ guardianRepository }: AddPet.Dependencies) {
+    this.guardianRepository = guardianRepository
+  }
+
   async add (petData: AddPet.Params): Promise<AddPet.Result> {
+    const guardian = await this.guardianRepository.loadById(petData.guardianId)
+    if (!guardian) {
+      return {
+        isSuccess: false,
+        error: new NotFoundError('userId')
+      }
+    }
     return {
-      isSuccess: false,
-      error: new NotFoundError('userId')
+      isSuccess: true,
+      specie: {
+        id: '1',
+        name: 'any_specie'
+      }
     }
   }
 }
