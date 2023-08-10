5987619164e0eef932e13f8d938e50e220ea1931
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-08-10 08:50:45 -0300

    test: ensure DbAddPet call loadById with correct values

--- tests/src/data/use-cases/pet/db-add-pet.spec.ts
+++ tests/src/data/use-cases/pet/db-add-pet.spec.ts
@@ -1,15 +1,22 @@
 import { NotFoundError } from '@/application/errors'
+import { type LoadGuardianByIdRepository } from '@/data/protocols'
 import { DbAddPet } from '@/data/use-cases'
 import { type AddPet } from '@/domain/use-cases'
+import { makeFakeGuardianRepository } from '@/tests/utils'
 
 interface SutTypes {
   sut: DbAddPet
+  guardianRepositoryStub: LoadGuardianByIdRepository
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new DbAddPet()
+  const guardianRepositoryStub = makeFakeGuardianRepository()
+  const sut = new DbAddPet({
+    guardianRepository: guardianRepositoryStub
+  })
   return {
-    sut
+    sut,
+    guardianRepositoryStub
   }
 }
 
@@ -19,8 +26,18 @@ describe('DbAddPet Use Case', () => {
     specieId: 'any_specie_id'
   }
   describe('GuardianRepository', () => {
+    it('Should call loadById method with correct values', async () => {
+      const { sut, guardianRepositoryStub } = makeSut()
+      const loadByEmailSpy = jest.spyOn(guardianRepositoryStub, 'loadById')
+
+      await sut.add(params)
+
+      expect(loadByEmailSpy).toHaveBeenCalledWith(params.guardianId)
+    })
+
     it('Should return not found error if incorrect id is provided', async () => {
-      const { sut } = makeSut()
+      const { sut, guardianRepositoryStub } = makeSut()
+      jest.spyOn(guardianRepositoryStub, 'loadById').mockResolvedValueOnce(undefined)
 
       const result = await sut.add(params)
 
