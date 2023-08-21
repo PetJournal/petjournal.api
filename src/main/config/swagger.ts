ebc2eb16a2a0ea0d79c6b2852ec01d47c7c2c0a4
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-21 20:39:43 -0300

    style: add dark mode to documentation

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,7 +1,12 @@
 import swaggerConfig from '@/main/docs'
 import { serve, setup } from 'swagger-ui-express'
 import { type Express } from 'express'
+import fs from 'fs'
+import path from 'path'
 
 export default (app: Express): void => {
+  app.use('/api/docs/dark', serve, setup(swaggerConfig, {
+    customCss: fs.readFileSync(path.join(__dirname, '..', '..', '..', 'styles', 'swagger-dark-theme.css'), 'utf-8')
+  }))
   app.use('/api/docs', serve, setup(swaggerConfig))
 }
