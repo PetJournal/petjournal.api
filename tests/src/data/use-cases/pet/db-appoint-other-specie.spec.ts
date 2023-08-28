c8d314e482989cd10662aeef6e818a023644b122
Rodrigo Meneses <rodrigofmeneses97@gmail.com>
2023-08-28 10:21:26 -0300

    test: remove redundants tests in DbAppointOtherSpecie

--- tests/src/data/use-cases/pet/db-appoint-other-specie.spec.ts
+++ tests/src/data/use-cases/pet/db-appoint-other-specie.spec.ts
@@ -30,38 +30,6 @@ describe('DbAppointOtherSpecie Use Case', () => {
     specieAlias: 'any_alias'
   }
 
-  describe('specie different of other', () => {
-    it('Should return undefined specieAlias when specieAlias is not provided', async () => {
-      const { sut } = makeSut()
-      const modifiedParams = {
-        ...params,
-        specieAlias: undefined
-      }
-
-      const result = await sut.appoint(modifiedParams)
-
-      expect(result).toEqual({
-        specieAppointed: result.specieAppointed,
-        specieAlias: undefined
-      })
-    })
-
-    it('Should return undefined specieAlias when specieAlias is provided', async () => {
-      const { sut } = makeSut()
-      const modifiedParams = {
-        ...params,
-        specieAlias: 'any_alias'
-      }
-
-      const result = await sut.appoint(modifiedParams)
-
-      expect(result).toEqual({
-        specieAppointed: result.specieAppointed,
-        specieAlias: undefined
-      })
-    })
-  })
-
   describe('LoadSpecies', () => {
     it('should return specieAppointed equal other when specieAlias is not equal to specie name in db', async () => {
       const { sut, specieRepositoryStub } = makeSut()
