b758cbc36c4ec8a53b616536ba44ebdff1300291
andersonnaz <anderson_1004@hotmail.com>
2023-09-01 10:57:19 -0300

    test: should return InvalidParamError if fieldName is not a valid name

--- tests/src/application/validation/validators/name-validation.spec.ts
+++ tests/src/application/validation/validators/name-validation.spec.ts
@@ -1,24 +1,43 @@
 import { InvalidParamError } from '@/application/errors'
 import { NameValidation, type NameValidator } from '@/application/validation'
 
-class NameValidatorStub implements NameValidator {
-  isValid (name: string): boolean {
-    return true
-  }
+interface SutTypes {
+  sut: NameValidation
+  nameValidatorStub: NameValidator
 }
 
-const makeSut = (): NameValidation => {
+const makeNameValidatorStub = (): NameValidator => {
+  class NameValidatorStub implements NameValidator {
+    isValid (name: string): boolean {
+      return true
+    }
+  }
+  return new NameValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
   const fakeFieldName: string = 'fieldName'
-  const nameValidatorStub = new NameValidatorStub()
+  const nameValidatorStub = makeNameValidatorStub()
   const sut = new NameValidation(fakeFieldName, nameValidatorStub)
-  return sut
+  return {
+    sut,
+    nameValidatorStub
+  }
 }
 
 describe('NameValidation', () => {
   it('should returns invalidParamError if fieldName is not a string', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const result = sut.validate({ fieldName: 11 })
+
+    expect(result).toStrictEqual(new InvalidParamError('fieldName'))
+  })
+
+  it('should return InvalidParamError if fieldName is not a valid name', () => {
+    const { sut, nameValidatorStub } = makeSut()
+    jest.spyOn(nameValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const result = sut.validate({ fieldName: 'invalid_name' })
 
     expect(result).toStrictEqual(new InvalidParamError('fieldName'))
   })
