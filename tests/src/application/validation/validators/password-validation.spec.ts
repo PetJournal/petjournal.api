1efbbcb37b47c0e31208abfe9bd00c05ae783d3c
andersonnaz <anderson_1004@hotmail.com>
2023-09-08 21:08:09 -0300

    test: ensure return InvalidParamError if fieldName is not a valid password

--- tests/src/application/validation/validators/password-validation.spec.ts
+++ tests/src/application/validation/validators/password-validation.spec.ts
@@ -1,23 +1,47 @@
 import { InvalidParamError } from '@/application/errors'
 import { PasswordValidation, type PasswordValidator } from '@/application/validation'
 
-const makesut = (): PasswordValidation => {
+const makePasswordValidatorStub = (): PasswordValidator => {
   class PasswordValidatorStub implements PasswordValidator {
     isValid (password: string): boolean {
       return true
     }
   }
+  return new PasswordValidatorStub()
+}
+
+interface SutTypes {
+  sut: PasswordValidation
+  passwordValidatorStub: PasswordValidator
+  fakeCustomError: undefined
+}
+
+const makesut = (): SutTypes => {
   const fakeFieldName: string = 'fieldName'
-  const passwordValidatorStub = new PasswordValidatorStub()
-  const sut = new PasswordValidation(fakeFieldName, passwordValidatorStub)
-  return sut
+  const fakeCustomError = undefined
+  const passwordValidatorStub = makePasswordValidatorStub()
+  const sut = new PasswordValidation(fakeFieldName, passwordValidatorStub, fakeCustomError)
+  return {
+    sut,
+    passwordValidatorStub,
+    fakeCustomError
+  }
 }
 
 describe('PasswordValidation', () => {
   it('should returns InvalidParamError if fieldName is not a string', () => {
-    const sut = makesut()
+    const { sut } = makesut()
 
     const result = sut.validate({ fieldName: 11 })
+
+    expect(result).toStrictEqual(new InvalidParamError('fieldName'))
+  })
+
+  it('should returns InvalidParamError if fieldName is not a valid password', () => {
+    const { sut, passwordValidatorStub } = makesut()
+    jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const result = sut.validate({ fieldName: 'invalid_password' })
 
     expect(result).toStrictEqual(new InvalidParamError('fieldName'))
   })
