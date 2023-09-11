a1cb3b1b4d68e0990287099008056a84bea97b6b
andersonnaz <anderson_1004@hotmail.com>
2023-09-11 08:52:21 -0300

    test: ensure returns Error if customError is provided

--- tests/src/application/validation/validators/compare-fields.spec.ts
+++ tests/src/application/validation/validators/compare-fields.spec.ts
@@ -1,24 +1,44 @@
 import { InvalidParamError } from '@/application/errors'
 import { CompareFieldsValidation } from '@/application/validation'
 
-const makeSut = (): CompareFieldsValidation => {
+interface SutTypes {
+  sut: CompareFieldsValidation
+  fakeFieldName: string
+  fakeFieldToCompareName: string
+}
+
+const makeSut = (): SutTypes => {
   const fakeFieldName: string = 'fieldName'
   const fakeFieldToCompareName: string = 'fieldToCompareName'
   const sut = new CompareFieldsValidation(fakeFieldName, fakeFieldToCompareName)
-  return sut
+  return {
+    sut,
+    fakeFieldName,
+    fakeFieldToCompareName
+  }
 }
 
 describe('CompareFieldsValidation', () => {
   it('should return InvalidParamError if fieldName is not equal than FieldToCompareName', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const result = sut.validate({ fieldName: 'fieldName', fieldToCompareName: 'invalid_fieldName' })
 
     expect(result).toStrictEqual(new InvalidParamError('fieldToCompareName'))
   })
 
+  it('should return Error if customError is provided', () => {
+    const { fakeFieldName, fakeFieldToCompareName } = makeSut()
+    const fakeCustomError = jest.fn(() => Error())
+    const sut = new CompareFieldsValidation(fakeFieldName, fakeFieldToCompareName, fakeCustomError)
+
+    const result = sut.validate({ fieldName: 'fieldName', fieldToCompareName: 'invalid_fieldName' })
+
+    expect(result).toStrictEqual(new Error())
+  })
+
   it('should return void if fieldName is equal FieldToCompareName', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const result = sut.validate({ fieldName: 'fieldName', fieldToCompareName: 'fieldName' })
 
