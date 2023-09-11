40fbfefd9b1c72514c8a1040860bed8f850bbef0
andersonnaz <anderson_1004@hotmail.com>
2023-09-11 14:38:41 -0300

    test: ensure returns MissingParamError if fieldName is not provided

---
+++ tests/src/application/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from '@/application/errors'
+import { RequiredFieldValidation } from '@/application/validation'
+
+const makeSut = (): RequiredFieldValidation => {
+  const fakeFieldName: string = 'fieldName'
+  const sut = new RequiredFieldValidation(fakeFieldName)
+  return sut
+}
+
+describe('RequiredFieldValidation', () => {
+  it('should return MissingParamError if fieldName is not provided', () => {
+    const sut = makeSut()
+
+    const result = sut.validate({ fieldName: '' })
+
+    expect(result).toStrictEqual(new MissingParamError('fieldName'))
+  })
+})
