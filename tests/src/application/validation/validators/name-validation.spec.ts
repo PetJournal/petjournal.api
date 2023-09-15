71914775544c34edfb65201a297367552aa3b610
andersonnaz <anderson_1004@hotmail.com>
2023-09-15 11:37:02 -0300

    refactor: organize tests in groups

--- tests/src/application/validation/validators/name-validation.spec.ts
+++ tests/src/application/validation/validators/name-validation.spec.ts
@@ -31,6 +31,32 @@ describe('NameValidation', () => {
     invalid: { fieldName: 'invalid_name' },
     notStringValue: { fieldName: 11 }
   }
+  describe('NameValidator', () => {
+    it('should return InvalidParamError if fieldName is not a valid name', () => {
+      const { sut, nameValidatorStub } = makeSut()
+      jest.spyOn(nameValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+      const result = sut.validate(params.invalid)
+
+      expect(result).toStrictEqual(new InvalidParamError('fieldName'))
+    })
+
+    it('should throw if validator throws', () => {
+      const { sut, nameValidatorStub } = makeSut()
+      jest.spyOn(nameValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
+
+      expect(() => { sut.validate(params.valid) }).toThrow()
+    })
+
+    it('should call validator with correct argument', () => {
+      const { sut, nameValidatorStub } = makeSut()
+      const isValidSpy = jest.spyOn(nameValidatorStub, 'isValid')
+
+      sut.validate(params.valid)
+
+      expect(isValidSpy).toHaveBeenCalledWith('valid_name')
+    })
+  })
 
   it('should returns invalidParamError if fieldName is not a string', () => {
     const { sut } = makeSut()
@@ -38,31 +64,6 @@ describe('NameValidation', () => {
     const result = sut.validate(params.notStringValue)
 
     expect(result).toStrictEqual(new InvalidParamError('fieldName'))
-  })
-
-  it('should return InvalidParamError if fieldName is not a valid name', () => {
-    const { sut, nameValidatorStub } = makeSut()
-    jest.spyOn(nameValidatorStub, 'isValid').mockReturnValueOnce(false)
-
-    const result = sut.validate(params.invalid)
-
-    expect(result).toStrictEqual(new InvalidParamError('fieldName'))
-  })
-
-  it('should throw if validator throws', () => {
-    const { sut, nameValidatorStub } = makeSut()
-    jest.spyOn(nameValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
-
-    expect(() => { sut.validate(params.valid) }).toThrow()
-  })
-
-  it('should call validator with correct argument', () => {
-    const { sut, nameValidatorStub } = makeSut()
-    const isValidSpy = jest.spyOn(nameValidatorStub, 'isValid')
-
-    sut.validate(params.valid)
-
-    expect(isValidSpy).toHaveBeenCalledWith('valid_name')
   })
 
   it('should return void if fieldName is a valid name', () => {
