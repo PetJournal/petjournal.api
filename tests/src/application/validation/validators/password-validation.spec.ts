32eb3986780e07fa0d4bf85da5405507385503d0
andersonnaz <anderson_1004@hotmail.com>
2023-09-15 11:41:20 -0300

    refactor: organize tests in groups

--- tests/src/application/validation/validators/password-validation.spec.ts
+++ tests/src/application/validation/validators/password-validation.spec.ts
@@ -34,48 +34,52 @@ describe('PasswordValidation', () => {
     notStringValue: { fieldName: 11 }
   }
 
+  describe('PasswordValidator', () => {
+    it('should returns InvalidParamError if fieldName is not a valid password', () => {
+      const { sut, passwordValidatorStub } = makesut()
+      jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+      const result = sut.validate(params.invalid)
+
+      expect(result).toStrictEqual(new InvalidParamError('fieldName'))
+    })
+
+    it('should call validator with a correct argument', () => {
+      const { sut, passwordValidatorStub } = makesut()
+      const spyIsValid = jest.spyOn(passwordValidatorStub, 'isValid')
+
+      sut.validate(params.valid)
+
+      expect(spyIsValid).toBeCalledWith('valid_password')
+    })
+
+    it('should throw if validator throws', () => {
+      const { sut, passwordValidatorStub } = makesut()
+      jest.spyOn(passwordValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
+
+      expect(() => { sut.validate(params.valid) }).toThrow()
+    })
+  })
+
+  describe('CustomError', () => {
+    it('should returns Error if customError is provided', () => {
+      const { fakeFieldName, passwordValidatorStub } = makesut()
+      const fakeCustomError = jest.fn(() => Error)
+      const sut = new PasswordValidation(fakeFieldName, passwordValidatorStub, fakeCustomError())
+      jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+      const result = sut.validate(params.invalid)
+
+      expect(result).toStrictEqual(new Error())
+    })
+  })
+
   it('should returns InvalidParamError if fieldName is not a string', () => {
     const { sut } = makesut()
 
     const result = sut.validate(params.notStringValue)
 
     expect(result).toStrictEqual(new InvalidParamError('fieldName'))
-  })
-
-  it('should returns InvalidParamError if fieldName is not a valid password', () => {
-    const { sut, passwordValidatorStub } = makesut()
-    jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
-
-    const result = sut.validate(params.invalid)
-
-    expect(result).toStrictEqual(new InvalidParamError('fieldName'))
-  })
-
-  it('should call validator with a correct argument', () => {
-    const { sut, passwordValidatorStub } = makesut()
-    const spyIsValid = jest.spyOn(passwordValidatorStub, 'isValid')
-
-    sut.validate(params.valid)
-
-    expect(spyIsValid).toBeCalledWith('valid_password')
-  })
-
-  it('should throw if validator throws', () => {
-    const { sut, passwordValidatorStub } = makesut()
-    jest.spyOn(passwordValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
-
-    expect(() => { sut.validate(params.valid) }).toThrow()
-  })
-
-  it('should returns Error if customError is provided', () => {
-    const { fakeFieldName, passwordValidatorStub } = makesut()
-    const fakeCustomError = jest.fn(() => Error)
-    const sut = new PasswordValidation(fakeFieldName, passwordValidatorStub, fakeCustomError())
-    jest.spyOn(passwordValidatorStub, 'isValid').mockReturnValueOnce(false)
-
-    const result = sut.validate(params.invalid)
-
-    expect(result).toStrictEqual(new Error())
   })
 
   it('should returns void if fieldName is a valid password', () => {
