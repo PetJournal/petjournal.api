69b44ad3cfd1a7e6f23d94560597fd5e36d8fd5c
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-30 19:41:49 -0300

    refactor: create function helper

--- tests/src/application/validation/validators/pet-gender-validation.spec.ts
+++ tests/src/application/validation/validators/pet-gender-validation.spec.ts
@@ -12,28 +12,32 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
+const makeFakeInput = (data: any): any => ({
+  gender: data
+})
+
 describe('PetGenderValidation', () => {
   it('Should return InvalidParamError if input is not a string', () => {
     const { sut } = makeSut()
-    const result = sut.validate(1)
+    const result = sut.validate(makeFakeInput(1))
     expect(result).toEqual(new InvalidParamError('gender'))
   })
 
   it("Should return InvalidParamError if input is not 'M' or 'F'", () => {
     const { sut } = makeSut()
-    const result = sut.validate('any_input')
+    const result = sut.validate(makeFakeInput('any_input'))
     expect(result).toEqual(new InvalidParamError('gender'))
   })
 
   it("Should return void if input is 'M'", () => {
     const { sut } = makeSut()
-    const result = sut.validate({ gender: 'M' })
+    const result = sut.validate(makeFakeInput('M'))
     expect(result).toBeFalsy()
   })
 
   it("Should return void if input is 'F'", () => {
     const { sut } = makeSut()
-    const result = sut.validate({ gender: 'F' })
+    const result = sut.validate(makeFakeInput('F'))
     expect(result).toBeFalsy()
   })
 })
