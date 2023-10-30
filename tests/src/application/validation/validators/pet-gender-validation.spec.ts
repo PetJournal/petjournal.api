ad112a5102e6fc34824bf5e149fcc76ca926f8aa
Matheus de Gondra <matheusgondra951@gmail.com>
2023-10-30 19:22:00 -0300

    test: ensure PetGenderValidation returns InvalidParamError if input is not a string

---
+++ tests/src/application/validation/validators/pet-gender-validation.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidParamError } from '@/application/errors'
+import { PetGenderValidation } from '@/application/validation'
+
+interface SutTypes {
+  sut: PetGenderValidation
+}
+
+const makeSut = (): SutTypes => {
+  const fieldName = 'gender'
+  const sut = new PetGenderValidation(fieldName)
+
+  return { sut }
+}
+
+describe('PetGenderValidation', () => {
+  it('Should return InvalidParamError if input is not a string', () => {
+    const { sut } = makeSut()
+    const input = 1
+    const result = sut.validate(input)
+    expect(result).toEqual(new InvalidParamError('gender'))
+  })
+})
