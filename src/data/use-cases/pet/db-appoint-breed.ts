8d239b84a461f65accfe1000eac113cbf5997508
andersonnaz <anderson_1004@hotmail.com>
2023-12-07 21:33:19 -0300

    feat: ensure DbAppointBreed returns breed with Outros if breedName is not found on db

---
+++ src/data/use-cases/pet/db-appoint-breed.ts
@@ -0,0 +1,26 @@
+import { type LoadBreedByNameRepository } from '@/data/protocols'
+import { type Breed } from '@/domain/models/breed'
+import { type AppointBreed } from '@/domain/use-cases'
+
+export class DbAppointBreed implements AppointBreed {
+  private readonly breedRepository: LoadBreedByNameRepository
+
+  constructor ({ breedRepository }: AppointBreed.Dependencies) {
+    this.breedRepository = breedRepository
+  }
+
+  async appoint (breedName: AppointBreed.Params): Promise<AppointBreed.Result> {
+    const breed = await this.breedRepository.loadByName(breedName)
+    if (!breed) {
+      const otherBreed = await this.breedRepository.loadByName('Outros')
+      return {
+        breed: otherBreed as Breed & { id: string },
+        breedAlias: breedName
+      }
+    }
+    return {
+      breed,
+      breedAlias: undefined
+    }
+  }
+}
