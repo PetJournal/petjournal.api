54cc8fcd557539fbfaaefefbe2aab5d0e58bebfc
andersonnaz <anderson_1004@hotmail.com>
2023-12-12 23:04:02 -0300

    test: ensure breedRepository returns a list of breeds on success

--- tests/src/data/use-cases/pet/breed/db-load-dog-breeds.spec.ts
+++ tests/src/data/use-cases/pet/breed/db-load-dog-breeds.spec.ts
@@ -28,5 +28,11 @@ describe('DbLoadDogBreeds', () => {
       const promise = sut.load()
       await expect(promise).rejects.toThrow()
     })
+
+    test('should return a list of breeds on success', async () => {
+      const { sut } = makeSut()
+      const result = await sut.load()
+      expect(result).toEqual([{ name: 'any_name' }])
+    })
   })
 })
