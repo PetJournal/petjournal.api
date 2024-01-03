5dbd93b593c6d1f1aac34cfe4757d778fcfe6d3c
Anderson Naz <anderson_1004@hotmail.com>
2024-01-03 16:56:53 -0300

    feat: update AddPetRepository protocol

--- src/data/protocols/db/pet/add-pet-repository.ts
+++ src/data/protocols/db/pet/add-pet-repository.ts
@@ -1,6 +1,8 @@
-import { type PetGender } from '@/domain/models/Pet'
+import { type PetGender } from '@/domain/models/pet'
 import { type Guardian } from '@/domain/models/guardian'
 import { type Specie } from '@/domain/models/specie'
+import { type Breed } from '@/domain/models/breed'
+import { type Size } from '@/domain/models/size'
 
 export interface AddPetRepository {
   add: (params: AddPetRepository.Params) => Promise<AddPetRepository.Result>
@@ -13,6 +15,9 @@ export namespace AddPetRepository {
     specieAlias?: string
     petName: string
     gender: PetGender
+    breedId: string
+    breedAlias: string
+    sizeId: string
   }
 
   type GuardianResultDb = Pick<Guardian, 'firstName' | 'lastName' | 'email' | 'phone'> & {
@@ -23,6 +28,14 @@ export namespace AddPetRepository {
     id: string
   }
 
+  type BreedResultDb = Breed & {
+    id: string
+  }
+
+  type SizeResultDb = Size & {
+    id: string
+  }
+
   export type Result = {
     id: string
     guardian: GuardianResultDb
@@ -30,5 +43,7 @@ export namespace AddPetRepository {
     specieAlias?: string | null
     petName: string
     gender: string
+    breed: BreedResultDb
+    size: SizeResultDb
   } | undefined
 }
