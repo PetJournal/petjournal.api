2aa3658cbdd76b5461184045743908e245708fdc
Anderson Naz <anderson_1004@hotmail.com>
2024-01-09 12:20:59 -0300

    test: ensure throw if validator throws

--- tests/src/application/validation/validators/breed-validation.spec.ts
+++ tests/src/application/validation/validators/breed-validation.spec.ts
@@ -20,10 +20,21 @@ const makeSut = (): SutTypes => {
   }
 }
 describe('BreedValidation', () => {
+  const params = {
+    valid: { fieldName: 'invalid_breedName' },
+    invalid: { fieldName: 'valid_breedName' },
+    notStringValue: { fieldName: 11 }
+  }
   test('should return InvalidParamError if breedName is not a valid breed', () => {
     const { sut, fakeBreedName, breedValidatorStub } = makeSut()
     jest.spyOn(breedValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const result = sut.validate({ breedName: 'invalid_breedName' })
+    const result = sut.validate(params.invalid)
     expect(result).toStrictEqual(new InvalidParamError(fakeBreedName))
+  })
+
+  test('should throw if validator throws', () => {
+    const { sut, breedValidatorStub } = makeSut()
+    jest.spyOn(breedValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
+    expect(() => { sut.validate(params.valid) }).toThrow()
   })
 })
