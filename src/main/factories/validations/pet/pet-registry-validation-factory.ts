c04f11ee175278b9f04f4b02bc81dd454ce99552
andersonnaz <anderson_1004@hotmail.com>
2024-01-10 00:15:16 -0300

    feat: add SizeValidation in ValidationComposite

--- src/main/factories/validations/pet/pet-registry-validation-factory.ts
+++ src/main/factories/validations/pet/pet-registry-validation-factory.ts
@@ -1,10 +1,10 @@
 import { type Validation } from '@/application/protocols'
-import { BreedValidation, NameValidation, PetGenderValidation, RequiredFieldValidation, ValidationComposite } from '@/application/validation'
-import { NameValidatorAdapter, BreedValidatorAdapter } from '@/infra/validators'
+import { BreedValidation, NameValidation, PetGenderValidation, RequiredFieldValidation, SizeValidation, ValidationComposite } from '@/application/validation'
+import { NameValidatorAdapter, BreedValidatorAdapter, SizeValidatorAdapter } from '@/infra/validators'
 
 export const makePetRegistryValidation = (): ValidationComposite => {
   const validations: Validation[] = []
-  const requiredFields = ['specieName', 'petName', 'gender', 'breedName']
+  const requiredFields = ['specieName', 'petName', 'gender', 'breedName', 'size']
 
   for (const field of requiredFields) {
     validations.push(new RequiredFieldValidation(field))
@@ -14,5 +14,6 @@ export const makePetRegistryValidation = (): ValidationComposite => {
   validations.push(new NameValidation('petName', new NameValidatorAdapter()))
   validations.push(new PetGenderValidation('gender'))
   validations.push(new BreedValidation('breedName', new BreedValidatorAdapter()))
+  validations.push(new SizeValidation('size', new SizeValidatorAdapter()))
   return new ValidationComposite(validations)
 }
