f1a55ff686552bc057ee55cf363ab4b733a07ac8
andersonnaz <anderson_1004@hotmail.com>
2024-01-10 00:13:10 -0300

    fix: solve regex problem in SizeValidationAdapter

--- src/infra/validators/size-validator-adapter.ts
+++ src/infra/validators/size-validator-adapter.ts
@@ -3,7 +3,7 @@ import validator from 'validator'
 
 export class SizeValidatorAdapter implements SizeValidator {
   isValid (size: string): boolean {
-    const isValidName = (field: string): boolean => validator.matches(field, /^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]{3,}$/)
+    const isValidName = (field: string): boolean => validator.matches(field, /^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ()]/)
     if (isValidName(size)) {
       return true
     }
