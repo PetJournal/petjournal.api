25cca4f962da1ac401dc5b3721af9b96f688c280
Anderson Naz <anderson_1004@hotmail.com>
2024-01-22 12:21:34 -0300

    fix: duplicante dependencie solved

--- src/data/use-cases/pet/db-appoint-pet.ts
+++ src/data/use-cases/pet/db-appoint-pet.ts
@@ -1,4 +1,4 @@
-import { type LoadBreedByNameRepository, type LoadSpecieByNameRepository } from '@/data/protocols'
+import { type LoadSizeByNameRepository, type LoadBreedByNameRepository, type LoadSpecieByNameRepository } from '@/data/protocols'
 import { type Breed } from '@/domain/models/breed'
 import { type Size } from '@/domain/models/size'
 import { type Specie } from '@/domain/models/specie'
@@ -7,7 +7,7 @@ import { type AppointPet } from '@/domain/use-cases'
 export class DbAppointPet implements AppointPet {
   private readonly specieRepository: LoadSpecieByNameRepository
   private readonly breedRepository: LoadBreedByNameRepository
-  private readonly sizeRepository: LoadSpecieByNameRepository
+  private readonly sizeRepository: LoadSizeByNameRepository
 
   constructor ({
     specieRepository,
