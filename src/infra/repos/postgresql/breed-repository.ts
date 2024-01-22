cf0e6547d5daaf47f64b165bf6a75632f15562dd
Anderson Naz <anderson_1004@hotmail.com>
2024-01-22 16:47:41 -0300

    fix: duplicate breed repository solved

--- src/infra/repos/postgresql/breed-repository.ts
+++ src/infra/repos/postgresql/breed-repository.ts
@@ -1,11 +1,18 @@
 import { prisma as db } from './prisma'
-import { type LoadBreedByNameRepository } from '@/data/protocols'
+import { type LoadCatBreedsRepository, type LoadBreedByNameRepository } from '@/data/protocols'
 
-export class BreedRepository implements LoadBreedByNameRepository {
+export class BreedRepository implements LoadBreedByNameRepository, LoadCatBreedsRepository {
   async loadByName (name: LoadBreedByNameRepository.Params): Promise<LoadBreedByNameRepository.Result> {
     const breed = await db.breed.findUnique({ where: { name } })
     if (breed) {
       return breed
+    }
+  }
+
+  async loadCatBreeds (): Promise<LoadCatBreedsRepository.Result> {
+    const catBreeds = await db.breed.findMany({ where: { specie: { name: 'Gato' } } })
+    if (catBreeds) {
+      return catBreeds
     }
   }
 }
