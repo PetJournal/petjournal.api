62c94c31f1ef0bb43a418a51a21500ca1dd2f2c6
Anderson Naz <anderson_1004@hotmail.com>
2024-01-26 16:53:35 -0300

    test: ensure return a list of cat breeds

--- tests/src/infra/repos/postgresql/breed-repository.spec.ts
+++ tests/src/infra/repos/postgresql/breed-repository.spec.ts
@@ -10,24 +10,68 @@ const makeSut = (): BreedRepository => {
 }
 
 describe('BreedRepository', () => {
-  it('Should not return a breed if invalid name is provided', async () => {
-    const sut = makeSut()
-    const name = 'invalid_name'
-    const result = await sut.loadByName(name)
-    expect(result).toBeFalsy()
+  describe('LoadByName method', () => {
+    it('Should not return a breed if invalid name is provided', async () => {
+      const sut = makeSut()
+      const name = 'invalid_name'
+      const result = await sut.loadByName(name)
+      expect(result).toBeFalsy()
+    })
+
+    it('Should return an breed if valid name is provided', async () => {
+      const sut = makeSut()
+      const specieName = 'any_name'
+      const specie = await db.specie.create({ data: { name: specieName } })
+      const breed = {
+        name: 'any_name',
+        specieId: specie.id
+      }
+      await db.breed.create({ data: { name: breed.name, specieId: breed.specieId } })
+      const result = await sut.loadByName(breed.name)
+      expect(result).toBeTruthy()
+      expect(result).toMatchObject({ name: 'any_name' })
+    })
   })
 
-  it('Should return an breed if valid name is provided', async () => {
-    const sut = makeSut()
-    const specieName = 'any_name'
-    const specie = await db.specie.create({ data: { name: specieName } })
-    const breed = {
-      name: 'any_name',
-      specieId: specie.id
-    }
-    await db.breed.create({ data: { name: breed.name, specieId: breed.specieId } })
-    const result = await sut.loadByName(breed.name)
-    expect(result).toBeTruthy()
-    expect(result).toMatchObject({ name: 'any_name' })
+  describe('LoadCatBreeds', () => {
+    it('Should return a list of cat breeds', async () => {
+      const sut = makeSut()
+      const specieName = 'Gato'
+      const specie = await db.specie.create({ data: { name: specieName } })
+      const breeds = [
+        {
+          name: 'any_name_1',
+          specieId: specie.id
+        },
+        {
+          name: 'any_name_2',
+          specieId: specie.id
+        },
+        {
+          name: 'any_name_3',
+          specieId: specie.id
+        }
+      ]
+      await db.breed.createMany({ data: breeds })
+      const result = await sut.loadCatBreeds()
+      expect(result).toBeTruthy()
+      expect(result).toEqual([
+        {
+          id: expect.any(String),
+          name: 'any_name_1',
+          specieId: specie.id
+        },
+        {
+          id: expect.any(String),
+          name: 'any_name_2',
+          specieId: specie.id
+        },
+        {
+          id: expect.any(String),
+          name: 'any_name_3',
+          specieId: specie.id
+        }
+      ])
+    })
   })
 })
