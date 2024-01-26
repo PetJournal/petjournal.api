8e01d3e3532727f92648c78b77ee9fc5ec4dde0e
Anderson Naz <anderson_1004@hotmail.com>
2024-01-26 16:01:20 -0300

    test: ensure return an breed if valid name is provided

--- tests/src/infra/repos/postgresql/breed-repository.spec.ts
+++ tests/src/infra/repos/postgresql/breed-repository.spec.ts
@@ -1,3 +1,4 @@
+import { prisma as db } from '@/infra/repos/postgresql/prisma'
 import { BreedRepository } from '@/infra/repos/postgresql'
 import { PrismaHelper } from '@/tests/helpers/prisma-helper'
 
@@ -14,5 +15,19 @@ describe('BreedRepository', () => {
     const name = 'invalid_name'
     const result = await sut.loadByName(name)
     expect(result).toBeFalsy()
+  })
+
+  it('Should return an breed if valid name is provided', async () => {
+    const sut = makeSut()
+    const specieName = 'any_name'
+    const specie = await db.specie.create({ data: { name: specieName } })
+    const breed = {
+      name: 'any_name',
+      specieId: specie.id
+    }
+    await db.breed.create({ data: { name: breed.name, specieId: breed.specieId } })
+    const result = await sut.loadByName(breed.name)
+    expect(result).toBeTruthy()
+    expect(result).toMatchObject({ name: 'any_name' })
   })
 })
