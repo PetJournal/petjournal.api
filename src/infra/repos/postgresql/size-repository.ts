454f7f8d336c5912b63a925d229a47129a511c77
andersonnaz <anderson_1004@hotmail.com>
2024-01-29 20:08:35 -0300

    feat: add LoadCatSizes method to return a list of cat sizes

--- src/infra/repos/postgresql/size-repository.ts
+++ src/infra/repos/postgresql/size-repository.ts
@@ -1,11 +1,18 @@
 import { prisma as db } from './prisma'
-import { type LoadSizeByNameRepository } from '@/data/protocols/db/size'
+import { type LoadCatSizesRepository, type LoadSizeByNameRepository } from '@/data/protocols/db/size'
 
-export class SizeRepository implements LoadSizeByNameRepository {
+export class SizeRepository implements LoadSizeByNameRepository, LoadCatSizesRepository {
   async loadByName (name: LoadSizeByNameRepository.Params): Promise<LoadSizeByNameRepository.Result> {
     const size = await db.size.findUnique({ where: { name } })
     if (size) {
       return size
+    }
+  }
+
+  async loadCatSizes (): Promise<LoadCatSizesRepository.Result> {
+    const sizes = await db.size.findMany({ where: { specie: { name: 'Gato' } } })
+    if (sizes) {
+      return sizes
     }
   }
 }
