d202686a2d22fa5e16d2538e42ab4441169dceab
andersonnaz <anderson_1004@hotmail.com>
2024-01-29 20:02:29 -0300

    feat: add index file to compile imports

--- src/data/use-cases/pet/index.ts
+++ src/data/use-cases/pet/index.ts
@@ -1,2 +1,4 @@
 export * from './db-add-pet'
 export * from './db-appoint-specie'
+export * from './breed'
+export * from './size'
---
+++ src/data/use-cases/pet/size/index.ts
@@ -0,0 +1,2 @@
+export * from './db-load-cat-sizes-repository'
+export * from './db-load-dog-sizes-repository'
