563b07ee3ef774bbde459888492bf2851646e865
andersonnaz <anderson_1004@hotmail.com>
2024-01-29 21:24:44 -0300

    feat: create cat and dog sizes factories

---
+++ src/main/factories/controllers/pet/load-dog-sizes-factory.ts
@@ -0,0 +1,15 @@
+import { type Controller } from '@/application/protocols'
+import { makeDbLoadDogSizes } from '../../usecases'
+import { LoadDogSizesController } from '@/application/controllers/pets/sizes/load-dog'
+import { LoggerPgRepository } from '@/infra/repos/postgresql'
+import { LoggerControllerDecorator } from '@/main/decorators'
+
+export const makeLoadDogSizesController = (): Controller => {
+  const loadDogSizes = makeDbLoadDogSizes()
+  const dependencies: LoadDogSizesController.Dependencies = {
+    loadDogSizes
+  }
+  const loadDogSizesController = new LoadDogSizesController(dependencies)
+  const loggerPgRepository = new LoggerPgRepository()
+  return new LoggerControllerDecorator(loadDogSizesController, loggerPgRepository)
+}
