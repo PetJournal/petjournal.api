65f51aabde8e1e7437f734d023e37d3918ac1ad3
matheusgondra <matheusgondra951@gmail.com>
2025-02-28 11:56:32 -0300

    refactor: remove upload middleware to global

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,9 +1,8 @@
 import { type Express } from 'express'
-import { bodyParser, contentType, cors, upload } from '@/main/middlewares'
+import { bodyParser, contentType, cors } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(contentType)
   app.use(cors)
-  app.use(upload)
 }
