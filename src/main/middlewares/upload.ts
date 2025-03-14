efe9d077b0adefdca674d80d64f48eb6cc48edfb
matheusgondra <matheusgondra951@gmail.com>
2025-03-14 16:37:24 -0300

    feat: ensure upload middleware throw if file is greater than 2MB

--- src/main/middlewares/upload.ts
+++ src/main/middlewares/upload.ts
@@ -1,5 +1,28 @@
+import type { NextFunction, Request, Response } from 'express'
 import multer from 'multer'
 
-const uploadMulter = multer()
+const BYTE = 1
+const KILOBYTE = 1024 * BYTE
+const MEGABYTE = 1024 * KILOBYTE
 
-export const upload = uploadMulter.single('image')
+const uploadMulter = multer({
+  limits: {
+    fileSize: 2 * MEGABYTE
+  }
+})
+
+export const upload = (req: Request, res: Response, next: NextFunction): void => {
+  const uploadSingle = uploadMulter.single('image')
+
+  uploadSingle(req, res, (err: any) => {
+    if (!err) {
+      next()
+    }
+
+    if (err instanceof multer.MulterError) {
+      return res.status(400).json({ error: err.message })
+    }
+
+    next(err)
+  })
+}
