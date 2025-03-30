9a4c971deb165a64585184b83aac12b238051ace
Anderson Naz <anderson_1004@hotmail.com>
2025-03-30 11:47:34 -0300

    feat: create tag repository

---
+++ src/infra/repos/postgresql/tag-repository.ts
@@ -0,0 +1,23 @@
+import { type AddTagRepository } from '@/data/protocols/db/tag/add-tag-repository'
+import { prisma as db } from './prisma'
+
+export class TagRepository implements AddTagRepository {
+  async add (params: AddTagRepository.Params): Promise<AddTagRepository.Result> {
+    try {
+      const tag = await db.tag.create({
+        data: {
+          name: params.name,
+          color: params.color
+        },
+        select: {
+          id: true,
+          name: true,
+          color: true
+        }
+      })
+      return tag
+    } catch (error) {
+      return undefined
+    }
+  }
+}
