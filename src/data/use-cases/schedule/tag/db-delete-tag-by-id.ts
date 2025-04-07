74523963adfb5ab1bb92371498b866972c7a8132
Anderson Naz <anderson_1004@hotmail.com>
2025-04-07 16:11:21 -0300

    feat: delete tag by id use case

---
+++ src/data/use-cases/schedule/tag/db-delete-tag-by-id.ts
@@ -0,0 +1,24 @@
+import { NotFoundError } from '@/application/errors'
+import { type DeleteTagRepository } from '@/data/protocols'
+import { type DeleteTagById } from '@/domain/use-cases/schedule/tag'
+
+export class DbDeleteTagById implements DeleteTagById {
+  private readonly tagRepository: DeleteTagRepository
+
+  constructor ({ tagRepository }: DeleteTagById.Dependencies) {
+    this.tagRepository = tagRepository
+  }
+
+  async deleteById (tagId: DeleteTagById.Param): Promise<DeleteTagById.Result> {
+    const tag = await this.tagRepository.deleteById(tagId)
+    if (!tag) {
+      return {
+        isSuccess: false,
+        error: new NotFoundError('tagId')
+      }
+    }
+    return {
+      isSuccess: true
+    }
+  }
+}
