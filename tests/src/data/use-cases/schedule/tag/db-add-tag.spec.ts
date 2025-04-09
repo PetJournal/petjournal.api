14b6cc1047868e25431bfd9b8f6f5157dfbb7e84
Anderson Naz <anderson_1004@hotmail.com>
2025-04-09 16:07:41 -0300

    test: ensure call add method with correct values

---
+++ tests/src/data/use-cases/schedule/tag/db-add-tag.spec.ts
@@ -0,0 +1,39 @@
+import { type AddTagRepository } from '@/data/protocols'
+import { DbAddTag } from '@/data/use-cases'
+import { type AddTag } from '@/domain/use-cases/schedule/tag/add-tag'
+import { makeFakeTagRepository } from '@/tests/utils'
+
+interface SutTypes {
+  sut: DbAddTag
+  tagRepositoryStub: AddTagRepository
+}
+
+const makeSut = (): SutTypes => {
+  const tagRepositoryStub = makeFakeTagRepository()
+  const dependencies: AddTag.Dependencies = {
+    tagRepository: tagRepositoryStub
+  }
+  const sut = new DbAddTag(dependencies)
+  return {
+    sut,
+    tagRepositoryStub
+  }
+}
+
+describe('DbAddTag use case', () => {
+  const params: AddTag.Params = {
+    name: 'any_name',
+    color: 'any_color'
+  }
+  describe('TagRepository', () => {
+    it('Should call add method with correct values', async () => {
+      const { sut, tagRepositoryStub } = makeSut()
+      const addTagSpy = jest.spyOn(tagRepositoryStub, 'add')
+      await sut.add(params)
+      expect(addTagSpy).toHaveBeenCalledWith({
+        name: 'any_name',
+        color: 'any_color'
+      })
+    })
+  })
+})
