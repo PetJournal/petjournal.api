67ea06f5aff7b96b9431c3b53e7e65481110aa6e
andersonnaz <anderson_1004@hotmail.com>
2025-04-10 10:26:05 -0300

    test: ensure call loadById method with correct value

---
+++ tests/src/data/use-cases/schedule/tag/db-load-tag-by-id.spec.ts
@@ -0,0 +1,33 @@
+import { type LoadTagByIdRepository } from '@/data/protocols'
+import { DbLoadTagById } from '@/data/use-cases/schedule/tag/db-load-tag-by-id'
+import { type LoadTagById } from '@/domain/use-cases/schedule/tag'
+import { makeFakeTagRepository } from '@/tests/utils'
+
+interface SutTypes {
+  sut: DbLoadTagById
+  tagRepositoryStub: LoadTagByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const tagRepositoryStub = makeFakeTagRepository()
+  const dependencies: LoadTagById.Dependencies = {
+    tagRepository: tagRepositoryStub
+  }
+  const sut = new DbLoadTagById(dependencies)
+  return {
+    sut,
+    tagRepositoryStub
+  }
+}
+
+describe('DbLoadTagById use case', () => {
+  const param: LoadTagByIdRepository.Param = 'any_id'
+  describe('TagRepository', () => {
+    it('Should call loadById method with correct value', async () => {
+      const { sut, tagRepositoryStub } = makeSut()
+      const loadTagSpy = jest.spyOn(tagRepositoryStub, 'loadById')
+      await sut.loadById(param)
+      expect(loadTagSpy).toHaveBeenCalledWith('any_id')
+    })
+  })
+})
