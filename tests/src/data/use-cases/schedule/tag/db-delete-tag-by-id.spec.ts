9d1a8cd64117540d2bc08a3b2b563d31b0aaa348
Anderson Naz <anderson_1004@hotmail.com>
2025-04-14 17:26:35 -0300

    test: ensure return NotFoundError if invalid tag id is provided

---
+++ tests/src/data/use-cases/schedule/tag/db-delete-tag-by-id.spec.ts
@@ -0,0 +1,36 @@
+import { NotFoundError } from '@/application/errors'
+import { type DeleteTagRepository } from '@/data/protocols'
+import { DbDeleteTagById } from '@/data/use-cases'
+import { type DeleteTagById } from '@/domain/use-cases/schedule/tag'
+import { makeFakeTagRepository } from '@/tests/utils'
+
+interface SutTypes {
+  sut: DbDeleteTagById
+  tagRepositoryStub: DeleteTagRepository
+}
+
+const makeSut = (): SutTypes => {
+  const tagRepositoryStub = makeFakeTagRepository()
+  const dependencies: DeleteTagById.Dependencies = {
+    tagRepository: tagRepositoryStub
+  }
+  const sut = new DbDeleteTagById(dependencies)
+  return {
+    sut,
+    tagRepositoryStub
+  }
+}
+
+describe('DbDeleteTagById use case', () => {
+  describe('TagRepository', () => {
+    it('Should return NotFoundError if invalid tag id is provided', async () => {
+      const { sut, tagRepositoryStub } = makeSut()
+      jest.spyOn(tagRepositoryStub, 'deleteById').mockResolvedValueOnce(false)
+      const result = await sut.deleteById('invalid_id')
+      expect(result).toEqual({
+        isSuccess: false,
+        error: new NotFoundError('tagId')
+      })
+    })
+  })
+})
