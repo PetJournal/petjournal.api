7616a3e9636f50ce290a2e75ccf14760277b7510
andersonnaz <anderson_1004@hotmail.com>
2025-04-22 10:32:21 -0300

    feat: create addEventRepository protocol

---
+++ src/data/protocols/db/event/add-event-repository.ts
@@ -0,0 +1,20 @@
+export interface AddEventRepository {
+  add: (params: AddEventRepository.Params) => Promise<AddEventRepository.Result>
+}
+
+export namespace AddEventRepository {
+  export type Params = {
+    schedulerId: string
+    start: Date
+    end: Date
+    date: Date
+  }
+
+  export type Result = {
+    id: string
+    schedulerId: string
+    start: Date
+    end: Date
+    date: Date
+  }
+}
