9cadc22921443a4bb2a23a2eef63106f21b09b6d
andersonnaz <anderson_1004@hotmail.com>
2025-04-22 13:28:33 -0300

    feat: create add method in SchedulerRepository

---
+++ src/infra/repos/postgresql/scheduler-repository.ts
@@ -0,0 +1,31 @@
+import { type AddSchedulerRepository } from '@/data/protocols/db/scheduler/add-scheduler-repository'
+import { prisma as db } from './prisma'
+
+export class SchedulerRepository implements AddSchedulerRepository {
+  async add (params: AddSchedulerRepository.Params): Promise<AddSchedulerRepository.Result> {
+    const connectPets = params.pets.map(pet => ({ id: pet.id }))
+    try {
+      const scheduler = await db.scheduler.create({
+        data: {
+          tagId: params.tagId,
+          title: params.title,
+          description: params.description,
+          note: params.note,
+          startAt: params.startAt,
+          endAt: params.endAt,
+          daysOfWeek: params.daysOfWeek,
+          daysOfMonth: params.daysOfMonth,
+          pets: {
+            connect: connectPets
+          }
+        },
+        include: {
+          pets: true
+        }
+      })
+      return scheduler
+    } catch (error) {
+      return undefined
+    }
+  }
+}
