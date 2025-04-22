c7567fe755eb58fc1dcb79464dfc4af50d478c0e
andersonnaz <anderson_1004@hotmail.com>
2025-04-22 13:25:44 -0300

    feat: create AddSchedulerRepository protocol

---
+++ src/data/protocols/db/scheduler/add-scheduler-repository.ts
@@ -0,0 +1,32 @@
+import { type Pet } from '@prisma/client'
+
+export interface AddSchedulerRepository {
+  add: (params: AddSchedulerRepository.Params) => Promise<AddSchedulerRepository.Result>
+}
+
+export namespace AddSchedulerRepository {
+  export type Params = {
+    tagId: string
+    title: string
+    description: string
+    note: string
+    startAt: Date
+    endAt: Date
+    daysOfWeek: number[]
+    daysOfMonth: number[]
+    pets: Pet[]
+  }
+
+  export type Result = {
+    id: string
+    tagId: string
+    title: string
+    description: string
+    note: string
+    startAt: Date
+    endAt: Date
+    daysOfWeek: number[]
+    daysOfMonth: number[]
+    pets: Pet[]
+  } | undefined
+}
