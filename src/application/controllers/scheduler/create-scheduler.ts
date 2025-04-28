d449cac2d2f0bfacde89b46b27c87eb9c08cdd25
Anderson Naz <anderson_1004@hotmail.com>
2025-04-28 15:30:26 -0300

    feat: add create scheduler controller

---
+++ src/application/controllers/scheduler/create-scheduler.ts
@@ -0,0 +1,60 @@
+import { badRequest, create, notAcceptable, serverError, type HttpRequest, type HttpResponse } from '@/application/helpers'
+import { type Controller, type Validation } from '@/application/protocols'
+import { type AddScheduler } from '@/domain/use-cases/scheduler'
+
+export class CreateSchedulerController implements Controller {
+  private readonly addScheduler: AddScheduler
+  private readonly validation: Validation
+
+  constructor ({ AddScheduler, validation }: CreateSchedulerController.Dependencies) {
+    this.addScheduler = AddScheduler
+    this.validation = validation
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { tagId, title, description, note, startAt, endAt, daysOfWeek, daysOfMonth, daily, pets } = httpRequest.body
+      const result = await this.addScheduler.add({
+        tagId,
+        title,
+        description,
+        note,
+        startAt,
+        endAt,
+        daysOfWeek,
+        daysOfMonth,
+        daily,
+        pets
+      })
+      if (!result.isSuccess) {
+        return notAcceptable(result.error as Error)
+      }
+      return create({
+        id: result.data?.id,
+        tagId: result.data?.tagId,
+        title: result.data?.title,
+        description: result.data?.description,
+        note: result.data?.note,
+        startAt: result.data?.startAt,
+        endAt: result.data?.endAt,
+        daysOfWeek: result.data?.daysOfWeek,
+        daysOfMonth: result.data?.daysOfMonth,
+        daily: result.data?.daily,
+        pets: result.data?.pets
+      })
+    } catch (error) {
+      return serverError(error as Error)
+    }
+  }
+}
+
+export namespace CreateSchedulerController {
+  export interface Dependencies {
+    validation: Validation
+    AddScheduler: AddScheduler
+  }
+}
