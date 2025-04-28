d7d208b9dd0259e948f0a228d115f4f5282974df
Anderson Naz <anderson_1004@hotmail.com>
2025-04-28 13:59:28 -0300

    feat: create loadByDateAndStart and addMany methods in EventRepository

--- src/infra/repos/postgresql/event-repository.ts
+++ src/infra/repos/postgresql/event-repository.ts
@@ -1,7 +1,8 @@
-import { type AddEventRepository } from '@/data/protocols'
+import { type AddEventRepository, type LoadEventByDateAndStartRepository } from '@/data/protocols'
 import { prisma as db } from './prisma'
+import { type AddManyEventsRepository } from '@/data/protocols/db/event/add-many-events-repository'
 
-export class EventRepository implements AddEventRepository {
+export class EventRepository implements AddEventRepository, LoadEventByDateAndStartRepository, AddManyEventsRepository {
   async add (params: AddEventRepository.Params): Promise<AddEventRepository.Result> {
     try {
       const event = await db.event.create({
@@ -15,6 +16,28 @@ export class EventRepository implements AddEventRepository {
       return event
     } catch (error) {
       return undefined
+    }
+  }
+
+  async loadByDateAndStart (params: LoadEventByDateAndStartRepository.Params): Promise<LoadEventByDateAndStartRepository.Result> {
+    const event = await db.event.findFirst({
+      where: {
+        date: params.date,
+        start: params.start
+      }
+    })
+    return event
+  }
+
+  async addMany (params: AddManyEventsRepository.Params): Promise<AddManyEventsRepository.Result> {
+    try {
+      await db.event.createMany({
+        data: params,
+        skipDuplicates: true
+      })
+      return true
+    } catch (error) {
+      return false
     }
   }
 }
