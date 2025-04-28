d563b7dd5c0f6eea570b716b3ec0f8de82b8f603
Anderson Naz <anderson_1004@hotmail.com>
2025-04-28 14:00:26 -0300

    feat: create events generator service

--- src/infra/service/events-generator-service.ts
+++ src/infra/service/events-generator-service.ts
@@ -1,6 +1,14 @@
 import { type EventsGenerator } from '@/data/protocols/service/events-generator'
+import { type LoadEventByDateAndStartRepository, type AddEventRepository, type AddManyEventsRepository } from '@/data/protocols'
+import { NotAcceptableError, ServerError } from '@/application/errors'
 
 export class EventsGeneratorService implements EventsGenerator {
+  private readonly eventRepository: AddEventRepository & LoadEventByDateAndStartRepository & AddManyEventsRepository
+
+  constructor ({ eventRepository }: EventsGenerator.Dependencies) {
+    this.eventRepository = eventRepository
+  }
+
   async generate ({ schedulerId, startAt, endAt, daysOfWeek, daysOfMonth, daily }: EventsGenerator.Params): Promise<EventsGenerator.Result> {
     const startAtDatePart = startAt.toISOString().split('T')[0]
     const startAtTimePart = startAt.toISOString().split('T')[1].split('.')[0]
@@ -12,16 +20,34 @@ export class EventsGeneratorService implements EventsGenerator {
       for (let i = 0; currentDate.getDate() <= endAt.getDate(); i++) {
         currentDate.setDate(currentDate.getDate() + i)
         if (daysOfWeek.includes(currentDate.getDay())) {
-          const event = {
-            schedulerId,
-            start: startAtTimePart,
-            end: endAtTimePart,
-            date: currentDate
+          const event = await this.eventRepository.loadByDateAndStart({ date: currentDate, start: startAtTimePart })
+          if (event) {
+            return {
+              isSuccess: false,
+              error: new NotAcceptableError('Conflict start event'),
+              data: event
+            }
           }
-          events.push(event)
+          events.push({
+            schedulerId,
+            start: new Date(startAtTimePart),
+            end: new Date(endAtTimePart),
+            date: currentDate
+          })
         }
       }
-      return events
+
+      const eventsAddManyResult = await this.eventRepository.addMany(events)
+      if (!eventsAddManyResult) {
+        return {
+          isSuccess: false,
+          error: new ServerError('Internal Server Error!')
+        }
+      }
+
+      return {
+        isSuccess: true
+      }
     }
 
     if (daysOfMonth) {
@@ -30,16 +56,34 @@ export class EventsGeneratorService implements EventsGenerator {
       for (let i = 0; currentDate.getDate() <= endAt.getDate(); i++) {
         currentDate.setDate(currentDate.getDate() + i)
         if (daysOfMonth.includes(currentDate.getDate())) {
-          const event = {
-            schedulerId,
-            start: startAtTimePart,
-            end: endAtTimePart,
-            date: currentDate
+          const event = await this.eventRepository.loadByDateAndStart({ date: currentDate, start: startAtTimePart })
+          if (event) {
+            return {
+              isSuccess: false,
+              error: new NotAcceptableError('Conflict start event'),
+              data: event
+            }
           }
-          events.push(event)
+          events.push({
+            schedulerId,
+            start: new Date(startAtTimePart),
+            end: new Date(endAtTimePart),
+            date: currentDate
+          })
         }
       }
-      return events
+
+      const eventsAddManyResult = await this.eventRepository.addMany(events)
+      if (!eventsAddManyResult) {
+        return {
+          isSuccess: false,
+          error: new ServerError('Internal Server Error!')
+        }
+      }
+
+      return {
+        isSuccess: true
+      }
     }
 
     if (daily) {
@@ -47,22 +91,60 @@ export class EventsGeneratorService implements EventsGenerator {
       const currentDate = new Date()
       for (let i = 0; currentDate.getDate() <= endAt.getDate(); i++) {
         currentDate.setDate(currentDate.getDate() + i)
-        const event = {
-          schedulerId,
-          start: startAtTimePart,
-          end: endAtTimePart,
-          date: currentDate
+        const event = await this.eventRepository.loadByDateAndStart({ date: currentDate, start: startAtTimePart })
+        if (event) {
+          return {
+            isSuccess: false,
+            error: new NotAcceptableError('Conflict start event'),
+            data: event
+          }
         }
-        events.push(event)
+        events.push({
+          schedulerId,
+          start: new Date(startAtTimePart),
+          end: new Date(endAtTimePart),
+          date: currentDate
+        })
       }
-      return events
+
+      const eventsAddManyResult = await this.eventRepository.addMany(events)
+      if (!eventsAddManyResult) {
+        return {
+          isSuccess: false,
+          error: new ServerError('Internal Server Error!')
+        }
+      }
+
+      return {
+        isSuccess: true
+      }
+    }
+    const event = await this.eventRepository.loadByDateAndStart({ date: new Date(startAtDatePart), start: startAtTimePart })
+    if (event) {
+      return {
+        isSuccess: false,
+        error: new NotAcceptableError('Conflict start event'),
+        data: event
+      }
     }
 
-    return [{
+    const eventAddResult = await this.eventRepository.add({
       schedulerId,
-      start: startAtTimePart,
-      end: endAtTimePart,
+      start: new Date(startAtTimePart),
+      end: new Date(endAtTimePart),
       date: new Date(startAtDatePart)
-    }]
+    })
+
+    if (!eventAddResult) {
+      return {
+        isSuccess: false,
+        error: new ServerError('Internal Server Error!')
+      }
+    }
+
+    return {
+      isSuccess: true,
+      data: eventAddResult
+    }
   }
 }
