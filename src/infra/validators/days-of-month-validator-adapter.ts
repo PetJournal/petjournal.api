0b3973fc9ce2c679f75360010779cfa26799ceea
Anderson Naz <anderson_1004@hotmail.com>
2025-05-02 17:20:56 -0300

    feat: create days of month validator adapter

---
+++ src/infra/validators/days-of-month-validator-adapter.ts
@@ -0,0 +1,15 @@
+import { type DaysOfMonthValidator } from '@/application/validation'
+
+export class DaysOfMonthValidatorAdapter implements DaysOfMonthValidator {
+  isValid (days: number[]): boolean {
+    for (let i = 0; i < days.length; i++) {
+      if (isNaN(days[i])) {
+        return false
+      }
+      if (!days.every(day => day >= 0 && day <= 31)) {
+        return false
+      }
+    }
+    return true
+  }
+}
