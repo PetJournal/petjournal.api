e415a2c061e69f343cbbbb113339dfcdca4835c9
Anderson Naz <anderson_1004@hotmail.com>
2025-05-02 17:21:09 -0300

    feat: create days of week validator adapter

---
+++ src/infra/validators/days-of-week-validator-adapter.ts
@@ -0,0 +1,15 @@
+import { type DaysOfWeekValidator } from '@/application/validation'
+
+export class DaysOfWeekValidatorAdapter implements DaysOfWeekValidator {
+  isValid (days: number[]): boolean {
+    for (let i = 0; i < days.length; i++) {
+      if (isNaN(days[i])) {
+        return false
+      }
+      if (!days.every(day => day >= 0 && day <= 6)) {
+        return false
+      }
+    }
+    return true
+  }
+}
