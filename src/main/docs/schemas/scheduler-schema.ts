c71e8055579ce9de9f63a8fa1dcfeb4cae690abe
Anderson Naz <anderson_1004@hotmail.com>
2025-05-06 14:44:50 -0300

    docs: create scheduler schema

---
+++ src/main/docs/schemas/scheduler-schema.ts
@@ -0,0 +1,36 @@
+export const schedulerSchema = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    tagId: { type: 'string' },
+    title: { type: 'string' },
+    description: { type: 'string' },
+    note: { type: 'string' },
+    startAt: { type: 'string' },
+    endAt: { type: 'string' },
+    daysOfWeek: {
+      type: 'array',
+      items: {
+        type: 'number'
+      },
+      nullable: true
+    },
+    daysOfMonth: {
+      type: 'array',
+      items: {
+        type: 'number'
+      },
+      nullable: true
+    },
+    daily: {
+      type: 'boolean',
+      nullable: true
+    },
+    pets: {
+      type: 'array',
+      items: {
+        $ref: '#/schemas/pet'
+      }
+    }
+  }
+}
