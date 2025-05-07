44628ba9fbef4a6a644f7f093232b0b579f48e61
andersonnaz <anderson_1004@hotmail.com>
2025-05-07 14:41:54 -0300

    feat: create DeleteTagByIdController

---
+++ src/application/controllers/scheduler/tag/delete-tag-by-id.ts
@@ -0,0 +1,33 @@
+import { notAcceptable, serverError, success, type HttpRequest, type HttpResponse } from '@/application/helpers'
+import { type Controller } from '@/application/protocols'
+import { type DeleteTagById } from '@/domain/use-cases/scheduler/tag'
+
+export class DeleteTagByIdController implements Controller {
+  private readonly deleteTag: DeleteTagById
+
+  constructor ({ deleteTag }: DeleteTagByIdController.Dependencies) {
+    this.deleteTag = deleteTag
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { tagId } = httpRequest.body
+      const result = await this.deleteTag.deleteById(tagId)
+      if (!result.isSuccess) {
+        return notAcceptable(result.error as Error)
+      }
+      return success({
+        message: 'Tag deleted',
+        tagId
+      })
+    } catch (error) {
+      return serverError(error as Error)
+    }
+  }
+}
+
+export namespace DeleteTagByIdController {
+  export type Dependencies = {
+    deleteTag: DeleteTagById
+  }
+}
