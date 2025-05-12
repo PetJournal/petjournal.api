e7682f98544d76b0310f27e6da73d910dc9505ec
andersonnaz <anderson_1004@hotmail.com>
2025-05-12 18:53:26 -0300

    feat: create PUT route update tag

--- src/main/routes/tag-routes.ts
+++ src/main/routes/tag-routes.ts
@@ -1,8 +1,9 @@
 import { type Router } from 'express'
 import { accountConfirmation, auth } from '../middlewares'
 import { adaptRoute } from '../adapters'
-import { makeAddTagController } from '../factories'
+import { makeAddTagController, makeUpdateTagController } from '../factories'
 
 export default (router: Router): void => {
   router.post('/tag', auth, accountConfirmation, adaptRoute(makeAddTagController()))
+  router.put('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeUpdateTagController()))
 }
