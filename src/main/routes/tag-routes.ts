015cc6a07fa0f5ee3ec1543301fdf732695f69bc
andersonnaz <anderson_1004@hotmail.com>
2025-05-15 11:16:35 -0300

    feat: create GET route load tags

--- src/main/routes/tag-routes.ts
+++ src/main/routes/tag-routes.ts
@@ -1,10 +1,11 @@
 import { type Router } from 'express'
 import { accountConfirmation, auth } from '../middlewares'
 import { adaptRoute } from '../adapters'
-import { makeAddTagController, makeLoadTagByIdController, makeUpdateTagController } from '../factories'
+import { makeAddTagController, makeLoadTagByIdController, makeLoadTagsController, makeUpdateTagController } from '../factories'
 
 export default (router: Router): void => {
   router.post('/tag', auth, accountConfirmation, adaptRoute(makeAddTagController()))
   router.put('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeUpdateTagController()))
   router.get('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeLoadTagByIdController()))
+  router.get('/tag', auth, accountConfirmation, adaptRoute(makeLoadTagsController()))
 }
