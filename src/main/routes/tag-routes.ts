1df47bf1fecfa25dd2e394ff3693cf4b4d18a8b3
andersonnaz <anderson_1004@hotmail.com>
2025-05-15 11:18:04 -0300

    feat: create DELETE route delete tag by id

--- src/main/routes/tag-routes.ts
+++ src/main/routes/tag-routes.ts
@@ -1,11 +1,12 @@
 import { type Router } from 'express'
 import { accountConfirmation, auth } from '../middlewares'
 import { adaptRoute } from '../adapters'
-import { makeAddTagController, makeLoadTagByIdController, makeLoadTagsController, makeUpdateTagController } from '../factories'
+import { makeAddTagController, makeDeleteTagByIdController, makeLoadTagByIdController, makeLoadTagsController, makeUpdateTagController } from '../factories'
 
 export default (router: Router): void => {
   router.post('/tag', auth, accountConfirmation, adaptRoute(makeAddTagController()))
   router.put('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeUpdateTagController()))
   router.get('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeLoadTagByIdController()))
   router.get('/tag', auth, accountConfirmation, adaptRoute(makeLoadTagsController()))
+  router.delete('/tag/:tagId', auth, accountConfirmation, adaptRoute(makeDeleteTagByIdController()))
 }
