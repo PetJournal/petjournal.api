dfc9ac34d2c08e7b86b56afa5381252a9cf15698
andersonnaz <anderson_1004@hotmail.com>
2025-05-20 12:00:25 -0300

    test: ensure return false if date is not a ISO8601 date

--- tests/src/infra/validators/startAt-validator-adapter.spec.ts
+++ tests/src/infra/validators/startAt-validator-adapter.spec.ts
@@ -1,9 +1,5 @@
 import { StartAtValidatorAdapter } from '@/infra/validators'
 
-jest.mock('validator', () => ({
-  isISO8601: jest.fn(() => true)
-}))
-
 const makeSut = (): StartAtValidatorAdapter => {
   return new StartAtValidatorAdapter()
 }
@@ -18,6 +14,12 @@ describe('StartAt Validator Adapter', () => {
   it('Should return false if date is a previous date', () => {
     const sut = makeSut()
     const isValid = sut.isValid('2000-01-01T00:00:00Z')
+    expect(isValid).toBe(false)
+  })
+
+  it('Should return false if date is not a ISO8601 date', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('01-01-2030')
     expect(isValid).toBe(false)
   })
 })
