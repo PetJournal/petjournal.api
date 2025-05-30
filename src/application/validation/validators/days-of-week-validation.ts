8ba7a3c1f9b745b23e37670dba17ecedcd7d52c1
Anderson Naz <anderson_1004@hotmail.com>
2025-05-30 19:11:01 -0300

    fix: change input type in DaysOfWeek validation

--- src/application/validation/validators/days-of-week-validation.ts
+++ src/application/validation/validators/days-of-week-validation.ts
@@ -11,16 +11,16 @@ export class DaysOfWeekValidation implements Validation {
     this.validator = validator
   }
 
-  validate (input: number[]): Error | void {
-    if (!input.length) {
+  validate (input: any): Error | void {
+    if (!input.daysOfWeek.length) {
       return new InvalidParamError(this.fieldDaysOfWeek)
     }
-    for (let i = 0; i < input.length; i++) {
-      if (typeof input[i] !== 'number') {
+    for (let i = 0; i < input.daysOfWeek.length; i++) {
+      if (typeof input.daysOfWeek[i] !== 'number') {
         return new InvalidParamError(this.fieldDaysOfWeek)
       }
     }
-    const isValid = this.validator.isValid(input)
+    const isValid = this.validator.isValid(input.daysOfWeek)
     if (!isValid) {
       return new InvalidParamError(this.fieldDaysOfWeek)
     }
