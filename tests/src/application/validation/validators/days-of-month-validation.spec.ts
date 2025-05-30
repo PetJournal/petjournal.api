96a34f3247e0e356dcced7dad2d49dfb5c2d2fec
Anderson Naz <anderson_1004@hotmail.com>
2025-05-30 19:43:25 -0300

    fix: change the daysOfMonth argument on the tests

--- tests/src/application/validation/validators/days-of-month-validation.spec.ts
+++ tests/src/application/validation/validators/days-of-month-validation.spec.ts
@@ -22,33 +22,33 @@ const makeSut = (): SutTypes => {
 describe('DaysOfMonth Validation', () => {
   it('Should return InvalidParamError if a invalid array of days is provided', () => {
     const { sut, fakeDaysOfMonthField } = makeSut()
-    const isValid = sut.validate([])
+    const isValid = sut.validate({ daysOfMonth: [] })
     expect(isValid).toEqual(new InvalidParamError(fakeDaysOfMonthField))
   })
 
   it('Should throw if validator throws', () => {
     const { sut, validatorStub } = makeSut()
     jest.spyOn(validatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
-    expect(() => { sut.validate([0, 4, 6]) }).toThrow()
+    expect(() => { sut.validate({ daysOfMonth: [0, 4, 6] }) }).toThrow()
   })
 
   it('Should call validator with correct values', () => {
     const { sut, validatorStub } = makeSut()
     const validatorSpy = jest.spyOn(validatorStub, 'isValid')
-    sut.validate([0])
+    sut.validate({ daysOfMonth: [0] })
     expect(validatorSpy).toHaveBeenCalledWith([0])
   })
 
   it('Should return InvalidParamError if validator returns false', () => {
     const { sut, fakeDaysOfMonthField, validatorStub } = makeSut()
     jest.spyOn(validatorStub, 'isValid').mockReturnValueOnce(false)
-    const isValid = sut.validate([0, 4, 50])
+    const isValid = sut.validate({ daysOfMonth: [0, 4, 50] })
     expect(isValid).toEqual(new InvalidParamError(fakeDaysOfMonthField))
   })
 
   it('Should return void if array of days is valid', () => {
     const { sut } = makeSut()
-    const isValid = sut.validate([0, 4, 30])
+    const isValid = sut.validate({ daysOfMonth: [1, 4, 31] })
     expect(isValid).toBeFalsy()
   })
 })
