a667b5edec9f2be436ee1a2bd33d1b8ab29c2aa5
Anderson Naz <anderson_1004@hotmail.com>
2025-06-05 06:14:15 -0300

    feat: add timezone calc in EndAtValidatorAdapter

--- src/infra/validators/endAt-validator-adapter.ts
+++ src/infra/validators/endAt-validator-adapter.ts
@@ -3,7 +3,9 @@ import validator from 'validator'
 
 export class EndAtValidatorAdapter implements DateValidator {
   isValid (date: string): boolean {
-    const dateOfValidation = new Date(date)
+    const dateWithoutTimeZone = new Date(date)
+    const timeZone = (+3 * 60 * 60 * 1000)
+    const dateOfValidation = new Date(dateWithoutTimeZone.getTime() + timeZone)
     if (!validator.isISO8601(date)) {
       return false
     }
