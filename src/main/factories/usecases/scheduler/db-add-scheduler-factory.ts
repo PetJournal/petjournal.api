5d11b9c0589b4383a394ffabf2e867ce169d2ac4
Anderson Naz <anderson_1004@hotmail.com>
2025-06-05 06:12:35 -0300

    feat: add luxon adapter in DbAddScheduler factory

--- src/main/factories/usecases/scheduler/db-add-scheduler-factory.ts
+++ src/main/factories/usecases/scheduler/db-add-scheduler-factory.ts
@@ -2,14 +2,15 @@ import { DbAddScheduler } from '@/data/use-cases/scheduler/db-add-scheduler'
 import { type AddScheduler } from '@/domain/use-cases'
 import { EventRepository, PetRepository, TagRepository } from '@/infra/repos/postgresql'
 import { SchedulerRepository } from '@/infra/repos/postgresql/scheduler-repository'
-import { EventsGeneratorService } from '@/infra/service'
+import { EventsGeneratorService, LuxonAdapter } from '@/infra/service'
 
 export const makeDbAddScheduler = (): AddScheduler => {
   const tagRepository = new TagRepository()
   const petRepository = new PetRepository()
   const schedulerRepository = new SchedulerRepository()
   const eventRepository = new EventRepository()
-  const eventGenerator = new EventsGeneratorService({ eventRepository })
+  const dateTime = new LuxonAdapter()
+  const eventGenerator = new EventsGeneratorService({ eventRepository, dateTime })
   const dependencies: AddScheduler.Dependencies = {
     tagRepository,
     eventGenerator,
