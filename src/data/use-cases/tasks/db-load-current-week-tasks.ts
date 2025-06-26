ce07834577b8a68e7e5c558ecc81be41734171f8
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-06-26 11:21:39 -0300

    feat: create implementation class db-load-current-week-tasks.ts

---
+++ src/data/use-cases/tasks/db-load-current-week-tasks.ts
@@ -0,0 +1,25 @@
+import { type LoadCurrentWeekTasks } from '@/domain/use-cases'
+import { type LoadTasksByDateRepository } from '@/data/protocols'
+
+export class DbLoadCurrentWeekTasks implements LoadCurrentWeekTasks {
+  constructor (
+    private readonly eventRepository: LoadTasksByDateRepository
+  ) {}
+
+  async load ({ date }: LoadCurrentWeekTasks.Params): Promise<LoadCurrentWeekTasks.Result> {
+    const start = new Date(date)
+    start.setUTCHours(0, 0, 0, 0)
+
+    const dayOfWeek = start.getUTCDay()
+    const daysUntilEndOfWeek = 6 - dayOfWeek
+
+    const end = new Date(start)
+    end.setUTCDate(start.getUTCDate() + daysUntilEndOfWeek)
+    end.setUTCHours(23, 59, 59, 999)
+
+    return await this.eventRepository.loadAllByCurrentDate({
+      start,
+      end
+    })
+  }
+}
