32cb378c439d5e978f0fa59d65e21acf6b743da5
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-06-29 18:47:13 -0300

    docs: add load task by month route in swagger

---
+++ src/main/docs/paths/tasks/load-current-month-tasks-path.ts
@@ -0,0 +1,44 @@
+export const loadCurrentMonthTasksPath = {
+  get: {
+    tags: ['task'],
+    summary: 'List tasks of current month',
+    security: [
+      {
+        bearerAuth: []
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  id: { type: 'string' },
+                  schedulerId: { type: 'string' },
+                  start: { type: 'string', format: 'date-time' },
+                  end: { type: 'string', format: 'date-time' }
+                }
+              }
+            }
+          }
+        }
+      },
+      401: {
+        description: 'Unauthorized',
+        content: {
+          'application/json': {
+            schema: { $ref: '#/schemas/error' },
+            example: {
+              error: 'Invalid or expired token'
+            }
+          }
+        }
+      },
+      500: { $ref: '#/components/serverError' }
+    }
+  }
+}
