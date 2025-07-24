24028a8db4c5d819104b7379795e174af5f68d91
matheusgondra <matheusgondra951@gmail.com>
2025-07-24 18:25:17 -0300

    test: ensure LoadCurrentMonthTasksController call Validation with correct value

--- tests/src/application/controllers/tasks/load-current-month-tasks-controller.spec.ts
+++ tests/src/application/controllers/tasks/load-current-month-tasks-controller.spec.ts
@@ -1,6 +1,8 @@
 import { LoadCurrentMonthTasksController } from '@/application/controllers'
-import { success, serverError, type HttpResponse } from '@/application/helpers'
+import { success, serverError, type HttpResponse, type HttpRequest } from '@/application/helpers'
+import { type Validation } from '@/application/protocols'
 import { type LoadCurrentMonthTasks } from '@/domain/use-cases'
+import { makeFakeValidation } from '@/tests/utils'
 
 const makeLoadCurrentMonthTasksUseCase = (): LoadCurrentMonthTasks => ({
   load: jest.fn().mockResolvedValue([
@@ -17,14 +19,17 @@ const makeFakeServerError = (): HttpResponse => {
 interface SutTypes {
   sut: LoadCurrentMonthTasksController
   loadCurrentMonthTasksStub: LoadCurrentMonthTasks
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
   const loadCurrentMonthTasksStub = makeLoadCurrentMonthTasksUseCase()
-  const sut = new LoadCurrentMonthTasksController({ loadCurrentMonthTasks: loadCurrentMonthTasksStub })
+  const validationStub = makeFakeValidation()
+  const sut = new LoadCurrentMonthTasksController({ loadCurrentMonthTasks: loadCurrentMonthTasksStub, validation: validationStub })
   return {
     sut,
-    loadCurrentMonthTasksStub
+    loadCurrentMonthTasksStub,
+    validationStub
   }
 }
 
@@ -38,29 +43,48 @@ describe('LoadCurrentMonthTasksController', () => {
     jest.useRealTimers()
   })
 
-  it('Should call LoadCurrentMonthTasks with current date at start of day UTC', async () => {
-    const { sut, loadCurrentMonthTasksStub } = makeSut()
-    const loadSpy = jest.spyOn(loadCurrentMonthTasksStub, 'load')
+  const httpRequest: HttpRequest = {
+    query: {
+      tagId: 'anyTagId'
+    }
+  }
 
-    const expectedStartOfDay = new Date('2025-06-18T00:00:00.000Z')
+  describe('Validation', () => {
+    it('Should call Validation with correct value', async () => {
+      const { sut, validationStub } = makeSut()
+      const validateSpy = jest.spyOn(validationStub, 'validate')
 
-    await sut.handle({})
-    expect(loadSpy).toHaveBeenCalledWith({ date: expectedStartOfDay })
+      await sut.handle(httpRequest)
+
+      expect(validateSpy).toHaveBeenCalledWith(httpRequest.query)
+    })
   })
 
-  it('Should return 500 if LoadCurrentMonthTasks throws', async () => {
-    const { sut, loadCurrentMonthTasksStub } = makeSut()
-    jest.spyOn(loadCurrentMonthTasksStub, 'load').mockRejectedValueOnce(new Error())
-    const httpResponse = await sut.handle({})
-    expect(httpResponse).toEqual(makeFakeServerError())
-  })
+  describe('LoadCurrentMonthTasks', () => {
+    it('Should call LoadCurrentMonthTasks with current date at start of day UTC', async () => {
+      const { sut, loadCurrentMonthTasksStub } = makeSut()
+      const loadSpy = jest.spyOn(loadCurrentMonthTasksStub, 'load')
 
-  it('Should return tasks on success', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle({})
-    expect(httpResponse).toEqual(success([
-      { id: 'task1', schedulerId: 'sched_1', start: expect.any(Date), end: expect.any(Date) },
-      { id: 'task2', schedulerId: 'sched_2', start: expect.any(Date), end: expect.any(Date) }
-    ]))
+      const expectedStartOfDay = new Date('2025-06-18T00:00:00.000Z')
+
+      await sut.handle({})
+      expect(loadSpy).toHaveBeenCalledWith({ date: expectedStartOfDay })
+    })
+
+    it('Should return 500 if LoadCurrentMonthTasks throws', async () => {
+      const { sut, loadCurrentMonthTasksStub } = makeSut()
+      jest.spyOn(loadCurrentMonthTasksStub, 'load').mockRejectedValueOnce(new Error())
+      const httpResponse = await sut.handle({})
+      expect(httpResponse).toEqual(makeFakeServerError())
+    })
+
+    it('Should return tasks on success', async () => {
+      const { sut } = makeSut()
+      const httpResponse = await sut.handle({})
+      expect(httpResponse).toEqual(success([
+        { id: 'task1', schedulerId: 'sched_1', start: expect.any(Date), end: expect.any(Date) },
+        { id: 'task2', schedulerId: 'sched_2', start: expect.any(Date), end: expect.any(Date) }
+      ]))
+    })
   })
 })
