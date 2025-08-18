351c0a1ae59c958cb18fa0956751654a83e6a7bc
andersonnaz <anderson_1004@hotmail.com>
2025-08-18 10:40:54 -0300

    feat: add update method in settings repository

--- src/infra/repos/postgresql/settings-repository.ts
+++ src/infra/repos/postgresql/settings-repository.ts
@@ -1,11 +1,30 @@
 import { prisma as db } from './prisma'
-import { type LoadSettingsRepository } from '@/data/protocols'
+import { type UpdateSettingsRepository, type LoadSettingsRepository } from '@/data/protocols'
 
-export class SettingsRepository implements LoadSettingsRepository {
+export class SettingsRepository implements LoadSettingsRepository, UpdateSettingsRepository {
   async loadAll (guardianId: LoadSettingsRepository.Param): Promise<LoadSettingsRepository.Result> {
     const settings = await db.settings.findMany({
       where: { guardianId },
       select: {
+        notificationEmail: true,
+        notificationMobile: true
+      }
+    })
+    return settings
+  }
+
+  async update (params: UpdateSettingsRepository.Params): Promise<UpdateSettingsRepository.Result> {
+    const { guardianId, notificationEmail, notificationMobile } = params
+    const settings = await db.settings.update({
+      data: {
+        notificationEmail,
+        notificationMobile
+      },
+      where: {
+        guardianId
+      },
+      select: {
+        guardianId: true,
         notificationEmail: true,
         notificationMobile: true
       }
