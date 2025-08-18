da580b488a5e7d4f2b724d850f0ebfb5fbeb8ac5
andersonnaz <anderson_1004@hotmail.com>
2025-08-18 10:51:38 -0300

    feat: create update settings controller

--- src/application/controllers/settings/index.ts
+++ src/application/controllers/settings/index.ts
@@ -1,1 +1,2 @@
 export * from './load-settings'
+export * from './update-settings'
---
+++ src/application/controllers/settings/update-settings.ts
@@ -0,0 +1,43 @@
+import { badRequest, notAcceptable, serverError, success, type HttpRequest, type HttpResponse } from '@/application/helpers'
+import { type Validation, type Controller } from '@/application/protocols'
+import { type UpdateSettings } from '@/domain/use-cases'
+
+export class UpdateSettingsController implements Controller {
+  private readonly updateSettings: UpdateSettings
+  private readonly validation: Validation
+
+  constructor ({ updateSettings, validation }: UpdateSettingsController.Dependencies) {
+    this.updateSettings = updateSettings
+    this.validation = validation
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate({ ...httpRequest.body })
+      if (error) {
+        return badRequest(error)
+      }
+      const guardianId = httpRequest.userId
+      const updateData = { ...httpRequest.body, guardianId }
+      const result = await this.updateSettings.update(updateData)
+      if (!result.isSuccess) {
+        return notAcceptable(result.error as Error)
+      }
+      return success({
+        guardianId: result.data?.guardianId,
+        notificationEmail: result.data?.notificationEmail,
+        notificationMobile: result.data?.notificationMobile
+      })
+    } catch (error) {
+      return serverError(error as Error)
+    }
+  }
+}
+
+export namespace UpdateSettingsController {
+  export type Dependencies = {
+    validation: Validation
+    updateSettings: UpdateSettings
+  }
+
+}
