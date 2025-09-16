01e9bd410b354a59967d7bc88021866fa322d26e
matheusgondra <matheusgondra951@gmail.com>
2025-09-16 11:02:49 -0300

    feat: add helmet middlware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { type Express } from 'express'
-import { bodyParser, contentType, cors } from '@/main/middlewares'
+import { bodyParser, contentType, cors, helmet } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(contentType)
   app.use(cors)
+  app.use(helmet)
 }
