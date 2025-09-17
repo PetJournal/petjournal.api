7772d18304a50ad5108202a2ba60fe353780c62d
matheusgondra <matheusgondra951@gmail.com>
2025-09-17 20:30:21 -0300

    chore: config default metrics

--- src/main/middlewares/prometheus.ts
+++ src/main/middlewares/prometheus.ts
@@ -1,4 +1,10 @@
 import type { RequestHandler } from 'express'
 import promBundle from 'express-prom-bundle'
 
-export const prometheus = promBundle({ includeMethod: true, includePath: true }) as unknown as RequestHandler
+export const prometheus = promBundle({
+  includeMethod: true,
+  includePath: true,
+  promClient: {
+    collectDefaultMetrics: {}
+  }
+}) as unknown as RequestHandler
