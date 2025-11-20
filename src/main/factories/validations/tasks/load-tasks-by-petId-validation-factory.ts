1c60f3a4ede95e331a68e84c44ea7119f09d325f
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-11-20 10:58:40 -0300

    feat: remove load tasks by pet id

--- src/main/factories/validations/tasks/load-tasks-by-petId-validation-factory.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationComposite, RequiredFieldValidation } from '@/application/validation'
-import { type Validation } from '@/application/protocols'
-
-export const makeLoadTasksByPetIdValidation = (): Validation => {
-  return new ValidationComposite([
-    new RequiredFieldValidation('petId')
-  ])
-}
