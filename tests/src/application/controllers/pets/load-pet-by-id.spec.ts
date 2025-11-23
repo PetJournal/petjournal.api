1a4bf229bf6bfcf97c6eaae019e56d474031a314
andersonnaz <anderson_1004@hotmail.com>
2025-11-23 16:42:14 -0300

    test: ensure return a pet on success

--- tests/src/application/controllers/pets/load-pet-by-id.spec.ts
+++ tests/src/application/controllers/pets/load-pet-by-id.spec.ts
@@ -1,6 +1,6 @@
 import { LoadPetByIdController } from '@/application/controllers'
 import { NotFoundError } from '@/application/errors'
-import { badRequest, type HttpRequest } from '@/application/helpers'
+import { badRequest, success, type HttpRequest } from '@/application/helpers'
 import { type LoadPetById } from '@/domain/use-cases'
 import { makeFakeLoadPetByIdUseCase, makeFakeServerError } from '@/tests/utils'
 
@@ -50,5 +50,33 @@ describe('Load pet by id Controller', () => {
     })
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new NotFoundError('petId')))
+  })
+
+  it('Should return a pet on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(success({
+      id: expect.any(String),
+      guardianId: expect.any(String),
+      specie: {
+        id: expect.any(String),
+        name: 'any_name'
+      },
+      specieAlias: '',
+      petName: 'any_pet_name',
+      gender: 'any_pet_gender',
+      breedAlias: '',
+      breed: {
+        id: 'any_id',
+        name: 'any_name'
+      },
+      size: {
+        id: 'any_id',
+        name: 'any_name'
+      },
+      castrated: false,
+      dateOfBirth: new Date(2000, 10, 23),
+      image: 'any_image_url'
+    }))
   })
 })
