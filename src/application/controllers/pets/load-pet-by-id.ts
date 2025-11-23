3548241b9936d77ba1c48d232956a0f3c56c8edb
andersonnaz <anderson_1004@hotmail.com>
2025-11-23 15:28:01 -0300

    feat: create load pet by id controller

---
+++ src/application/controllers/pets/load-pet-by-id.ts
@@ -0,0 +1,27 @@
+import { serverError, success, type HttpRequest, type HttpResponse } from '@/application/helpers'
+import { type Controller } from '@/application/protocols'
+import { type LoadPetById } from '@/domain/use-cases'
+
+export class LoadPetByIdController implements Controller {
+  private readonly loadPet: LoadPetById
+
+  constructor ({ loadPet }: LoadPetByIdController.Dependencies) {
+    this.loadPet = loadPet
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { petId } = httpRequest.params
+      const result = await this.loadPet.loadById({ petId })
+      return success(result)
+    } catch (error) {
+      return serverError(error as Error)
+    }
+  }
+}
+
+export namespace LoadPetByIdController {
+  export type Dependencies = {
+    loadPet: LoadPetById
+  }
+}
