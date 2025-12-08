755e1da4d4bf9f2b5cfb9f74b0d7da7e0a4606eb
andersonnaz <anderson_1004@hotmail.com>
2025-12-08 17:30:40 -0300

    feat: create in event repository load tasks by petid and tagid

---
+++ src/data/protocols/db/task/load-next-task-by-petId-and-tagId-repository.ts
@@ -0,0 +1,43 @@
+export interface LoadNextTasksByPetIdAndTagIdRepository {
+  loadByPetIdAndTagId: (params: LoadNextTasksByPetIdAndTagIdRepository.Params) => Promise<LoadNextTasksByPetIdAndTagIdRepository.Result>
+}
+
+export namespace LoadNextTasksByPetIdAndTagIdRepository {
+  export type Params = {
+    petId: string
+    tagId: string
+    page?: number
+    limit?: number
+  }
+
+  export type Result = {
+    page: number
+    limit: number
+    totalPages: number
+    events: Array<{
+      id: string
+      start: Date
+      end: Date
+      schedulerId: string
+      scheduler: {
+        id: string
+        title: string
+        description: string
+        note: string
+        startAt: Date
+        endAt: Date
+        daysOfWeek: number[]
+        daysOfMonth: number[]
+        daily: boolean
+        tag: {
+          name: string
+          color: string
+        }
+        pets: Array<{
+          id: string
+          image: string
+        }>
+      }
+    }>
+  }
+}
