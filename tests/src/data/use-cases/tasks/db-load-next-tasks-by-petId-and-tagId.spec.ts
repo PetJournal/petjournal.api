f3a1caf18254a1ae533729df582dc09a43eae1ec
andersonnaz <anderson_1004@hotmail.com>
2025-12-13 20:30:01 -0300

    test: ensure call pet repository with correct value

---
+++ tests/src/data/use-cases/tasks/db-load-next-tasks-by-petId-and-tagId.spec.ts
@@ -0,0 +1,45 @@
+import { type LoadNextTasksByPetIdAndTagIdRepository, type LoadPetByIdRepository, type LoadTagByIdRepository } from '@/data/protocols'
+import { DbLoadNextTasksByPetIdAndTagId } from '@/data/use-cases'
+import { type LoadNextTasksByPetIdAndTagId } from '@/domain/use-cases'
+import { makeFakeEventRepository, makeFakePetRepository, makeFakeTagRepository } from '@/tests/utils'
+
+interface SutTypes {
+  sut: DbLoadNextTasksByPetIdAndTagId
+  petRepositoryStub: LoadPetByIdRepository
+  tagRepositoryStub: LoadTagByIdRepository
+  eventRepositoryStub: LoadNextTasksByPetIdAndTagIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const petRepositoryStub = makeFakePetRepository()
+  const tagRepositoryStub = makeFakeTagRepository()
+  const eventRepositoryStub = makeFakeEventRepository()
+  const dependencies: LoadNextTasksByPetIdAndTagId.Dependencies = {
+    petRepository: petRepositoryStub,
+    tagRepository: tagRepositoryStub,
+    eventRepository: eventRepositoryStub
+  }
+  const sut = new DbLoadNextTasksByPetIdAndTagId(dependencies)
+  return {
+    sut,
+    petRepositoryStub,
+    tagRepositoryStub,
+    eventRepositoryStub
+  }
+}
+
+describe('DbLoadNextTasksByPetIdAndTagId', () => {
+  const params: LoadNextTasksByPetIdAndTagId.Params = {
+    petId: 'any_pet_id',
+    tagId: 'any_tag_id',
+    limit: 10,
+    page: 1
+  }
+
+  it('Should call pet repository with correct value', async () => {
+    const { sut, petRepositoryStub } = makeSut()
+    const petRepositorySpy = jest.spyOn(petRepositoryStub, 'loadById')
+    await sut.load(params)
+    expect(petRepositorySpy).toHaveBeenCalledWith(params.petId)
+  })
+})
