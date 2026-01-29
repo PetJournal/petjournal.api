3cf49b1810e2fefb4eb9ca67b61cbca9a4ac7ee2
matheusgondra <matheusgondra951@gmail.com>
2026-01-29 13:58:00 -0300

    feat: ensure LoadGuardianNameController call logger.error if LoadGuardianName throws

--- src/application/controllers/load-guardian-name.ts
+++ src/application/controllers/load-guardian-name.ts
@@ -1,20 +1,26 @@
 import { type LoadGuardianName } from '@/domain/use-cases'
 import { success, type HttpRequest, type HttpResponse, serverError } from '@/application/helpers'
 import { type Controller } from '@/application/protocols'
+import { type Logger } from '@/data/protocols'
 
 export class LoadGuardianNameController implements Controller {
   private readonly loadGuardianName: LoadGuardianName
+  private readonly logger: Logger
 
-  constructor ({ loadGuardianName }: LoadGuardianNameController.Dependencies) {
+  constructor ({ loadGuardianName, logger }: LoadGuardianNameController.Dependencies) {
     this.loadGuardianName = loadGuardianName
+    this.logger = logger
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.logger.debug('httpRequest', { httpRequest })
       const guardianName = await this.loadGuardianName.load(httpRequest.userId as string)
       return success(guardianName)
     } catch (error) {
-      return serverError(error as Error)
+      const exception = error instanceof Error ? error : new Error(String(error))
+      this.logger.error(exception.message, exception)
+      return serverError(exception)
     }
   }
 }
@@ -22,5 +28,6 @@ export class LoadGuardianNameController implements Controller {
 export namespace LoadGuardianNameController {
   export interface Dependencies {
     loadGuardianName: LoadGuardianName
+    logger: Logger
   }
 }
--- src/main/factories/controllers/load-guardian-name-factory.ts
+++ src/main/factories/controllers/load-guardian-name-factory.ts
@@ -3,10 +3,12 @@ import { makeDbLoadGuardianName } from '@/main/factories/usecases'
 import { DevLoggerControllerDecorator, LoggerControllerDecorator } from '@/main/decorators'
 import { LoggerPgRepository } from '@/infra/repos/postgresql'
 import { type Controller } from '@/application/protocols'
+import { PinoAdapter } from '@/infra/log/pino-adapter'
 
 export const makeLoadGuardianNameController = (): Controller => {
   const loadGuardianName = makeDbLoadGuardianName()
-  const loadGuardianNameController = new LoadGuardianNameController({ loadGuardianName })
+  const logger = new PinoAdapter(LoadGuardianNameController.name)
+  const loadGuardianNameController = new LoadGuardianNameController({ loadGuardianName, logger })
   const loggerPgRepository = new LoggerPgRepository()
   const loggerControllerDecorator = new LoggerControllerDecorator(loadGuardianNameController, loggerPgRepository)
   return new DevLoggerControllerDecorator(loggerControllerDecorator)
