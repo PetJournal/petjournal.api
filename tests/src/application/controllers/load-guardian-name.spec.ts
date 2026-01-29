ef57671aad9def11bac7f016915b92ac0f2656bd
matheusgondra <matheusgondra951@gmail.com>
2026-01-29 14:00:38 -0300

    test: ensure LoadGuardianNameController call logger.error if LoadGuardianName throws

--- tests/src/application/controllers/load-guardian-name.spec.ts
+++ tests/src/application/controllers/load-guardian-name.spec.ts
@@ -1,40 +1,59 @@
 import { type LoadGuardianName } from '@/domain/use-cases'
 import { LoadGuardianNameController } from '@/application/controllers'
-import { success } from '@/application/helpers'
-import { makeFakeServerError, makeLoadGuardianNameUseCase } from '@/tests/utils'
+import { type HttpRequest, success } from '@/application/helpers'
+import { makeFakeLogger, makeFakeServerError, makeLoadGuardianNameUseCase } from '@/tests/utils'
+import { type Logger } from '@/data/protocols'
 
 interface SutTypes {
   sut: LoadGuardianNameController
   loadGuardianNameStub: LoadGuardianName
+  loggerStub: Logger
 }
 
 const makeSut = (): SutTypes => {
   const loadGuardianNameStub = makeLoadGuardianNameUseCase()
-  const sut = new LoadGuardianNameController({ loadGuardianName: loadGuardianNameStub })
+  const loggerStub = makeFakeLogger()
+  const sut = new LoadGuardianNameController({ loadGuardianName: loadGuardianNameStub, logger: loggerStub })
   return {
     sut,
-    loadGuardianNameStub
+    loadGuardianNameStub,
+    loggerStub
   }
 }
 
 describe('LoadGuardianName Controller', () => {
+  const httpRequest: HttpRequest = {
+    userId: 'any_user_id'
+  }
+
+  describe('Logger', () => {
+    it('Should call logger.error if LoadGuardianName throws', async () => {
+      const { sut, loadGuardianNameStub, loggerStub } = makeSut()
+      const loggerSpy = jest.spyOn(loggerStub, 'error')
+      const error = new Error('any_error')
+      jest.spyOn(loadGuardianNameStub, 'load').mockRejectedValueOnce(error)
+      await sut.handle(httpRequest)
+      expect(loggerSpy).toHaveBeenCalledWith(error.message, error)
+    })
+  })
+
   it('Should call LoadGuardianName with correct values', async () => {
     const { sut, loadGuardianNameStub } = makeSut()
     const getGuardianNameSpy = jest.spyOn(loadGuardianNameStub, 'load')
-    await sut.handle({ userId: 'any_user_id' })
+    await sut.handle(httpRequest)
     expect(getGuardianNameSpy).toHaveBeenCalledWith('any_user_id')
   })
 
   it('Should returns 500 (ServerError) if LoadGuardianName throws', async () => {
     const { sut, loadGuardianNameStub } = makeSut()
     jest.spyOn(loadGuardianNameStub, 'load').mockRejectedValueOnce(new Error())
-    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(makeFakeServerError())
   })
 
   it('Should return guardian name on success', async () => {
     const { sut } = makeSut()
-    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(success({ firstName: 'any_first_name', lastName: 'any_last_name' }))
   })
 })
