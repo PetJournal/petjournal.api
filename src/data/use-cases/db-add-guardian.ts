3826a63a8a40c415da176ddfdbb96129ca8df580
andersonnaz <anderson_1004@hotmail.com>
2026-02-09 09:58:49 -0300

    feat: updated to receive image on create guardian use case implementation

--- src/data/use-cases/db-add-guardian.ts
+++ src/data/use-cases/db-add-guardian.ts
@@ -1,17 +1,39 @@
 import { type AddGuardian } from '@/domain/use-cases'
-import { type AddGuardianRepository, type HashGenerator } from '@/data/protocols'
+import { type FileStorage, type AddGuardianRepository, type HashGenerator, type UpdateGuardianImageRepository } from '@/data/protocols'
 
 export class DbAddGuardian implements AddGuardian {
-  private readonly guardianRepository: AddGuardianRepository
+  private readonly guardianRepository: AddGuardianRepository & UpdateGuardianImageRepository
   private readonly hashService: HashGenerator
+  private readonly fileStorage: FileStorage
+  private readonly defaultGuardianImageUrl: string
 
-  constructor ({ guardianRepository, hashService }: AddGuardian.Dependencies) {
+  constructor ({ guardianRepository, hashService, fileStorage, defaultGuardianImageUrl }: AddGuardian.Dependencies) {
     this.guardianRepository = guardianRepository
     this.hashService = hashService
+    this.fileStorage = fileStorage
+    this.defaultGuardianImageUrl = defaultGuardianImageUrl
   }
 
   async add (guardianData: AddGuardian.Params): Promise<AddGuardian.Result> {
     const hashedPassword = await this.hashService.encrypt({ value: guardianData.password })
-    return await this.guardianRepository.add(Object.assign({}, guardianData, { password: hashedPassword }))
+    const guardian = await this.guardianRepository.add(Object.assign({}, { ...guardianData, image: '' }, { password: hashedPassword }))
+
+    let imageUrl: string = ''
+    if (guardianData.image) {
+      imageUrl = await this.fileStorage.save({ file: guardianData.image, fileName: `images/guardian-${guardian?.id as string}` })
+    }
+
+    if (imageUrl) {
+      await this.guardianRepository.updateImage({ guardianId: guardian?.id as string, image: imageUrl })
+    }
+
+    return {
+      id: guardian?.id as string,
+      email: guardian?.email as string,
+      firstName: guardian?.firstName as string,
+      lastName: guardian?.lastName as string,
+      phone: guardian?.phone as string,
+      image: imageUrl ?? this.defaultGuardianImageUrl
+    }
   }
 }
