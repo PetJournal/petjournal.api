78f5c1c08265c52dc2ec30bb1d15f9cc8528e3ce
andersonnaz <anderson_1004@hotmail.com>
2026-02-09 10:02:16 -0300

    feat: add upload middleware on signup route

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
 import { type Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
 import { makeSignUpController } from '@/main/factories'
+import { upload } from '../middlewares'
 
 export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/signup', upload, adaptRoute(makeSignUpController()))
 }
