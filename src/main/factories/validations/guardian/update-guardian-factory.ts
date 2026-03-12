1648611f5a22979c25244e2adac3c2cb61d99296
andersonnaz <anderson_1004@hotmail.com>
2026-03-12 16:07:50 -0300

    fix: change class to validate phone

--- src/main/factories/validations/guardian/update-guardian-factory.ts
+++ src/main/factories/validations/guardian/update-guardian-factory.ts
@@ -1,13 +1,13 @@
 import { type Validation } from '@/application/protocols'
-import { NameValidation, OptionalFieldValidation, ValidationComposite } from '@/application/validation'
-import { NameValidatorAdapter } from '@/infra/validators'
+import { NameValidation, OptionalFieldValidation, PhoneValidation, ValidationComposite } from '@/application/validation'
+import { NameValidatorAdapter, PhoneValidatorAdapter } from '@/infra/validators'
 
 export const makeUpdateGuardianValidation = (): ValidationComposite => {
   const validations: Validation[] = []
 
   validations.push(new OptionalFieldValidation('firstName', new NameValidation('firstName', new NameValidatorAdapter())))
   validations.push(new OptionalFieldValidation('lastName', new NameValidation('lastName', new NameValidatorAdapter())))
-  validations.push(new OptionalFieldValidation('phone', new NameValidation('phone', new NameValidatorAdapter())))
+  validations.push(new OptionalFieldValidation('phone', new PhoneValidation('phone', new PhoneValidatorAdapter())))
 
   return new ValidationComposite(validations)
 }
