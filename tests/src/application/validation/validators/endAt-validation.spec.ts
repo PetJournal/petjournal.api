ed508e47ff97686ed1f92f00c2d7ce561ebed097
andersonnaz <anderson_1004@hotmail.com>
2026-04-03 22:43:03 -0300

    test: add startAt in validator tests

--- tests/src/application/validation/validators/endAt-validation.spec.ts
+++ tests/src/application/validation/validators/endAt-validation.spec.ts
@@ -10,8 +10,9 @@ interface SutTypes {
 
 const makeSut = (): SutTypes => {
   const fakeEndAtField = 'endAt'
+  const fakeStartAtField = 'startAt'
   const validatorStub = makeFakeDateValidator()
-  const sut = new EndAtValidation(fakeEndAtField, validatorStub)
+  const sut = new EndAtValidation(fakeEndAtField, fakeStartAtField, validatorStub)
   return {
     sut,
     fakeEndAtField,
@@ -36,8 +37,8 @@ describe('EndAt Validation', () => {
   it('Should call validator with correct values', () => {
     const { sut, validatorStub } = makeSut()
     const validatorSpy = jest.spyOn(validatorStub, 'isValid')
-    sut.validate({ endAt: 'any_date' })
-    expect(validatorSpy).toHaveBeenCalledWith('any_date')
+    sut.validate({ startAt: 'any_start_date', endAt: 'any_end_date' })
+    expect(validatorSpy).toHaveBeenCalledWith('any_start_date', 'any_end_date')
   })
 
   it('Should return InvalidParamError if EndAt is not a date', () => {
--- tests/src/infra/validators/endAt-validator-adapter.spec.ts
+++ tests/src/infra/validators/endAt-validator-adapter.spec.ts
@@ -5,33 +5,38 @@ const makeSut = (): EndAtValidatorAdapter => {
 }
 
 describe('EndAt Validator Adapter', () => {
+  const params = {
+    startAt: '2000-01-01T00:00:00',
+    endAt: '2000-01-02T00:00:00'
+  }
+
   it('Should return false if time in date is not valid', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('invalid_date')
+    const isValid = sut.isValid(params.startAt, 'invalid_date')
     expect(isValid).toBe(false)
   })
 
   it('Should return false if in endAt is not ends with Z', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('2000-01-01T00:00:00')
+    const isValid = sut.isValid(params.startAt, params.endAt)
     expect(isValid).toBe(false)
   })
 
   it('Should return false if date is a previous date', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('2000-01-01T00:00:00Z')
+    const isValid = sut.isValid(params.startAt, params.endAt)
     expect(isValid).toBe(false)
   })
 
   it('Should return false if date is not a ISO8601 date', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('01-01-2025')
+    const isValid = sut.isValid(params.startAt, '01-01-2025')
     expect(isValid).toBe(false)
   })
 
   it('Should return true if validation returns true', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('2030-05-19T00:00:00Z')
+    const isValid = sut.isValid('2030-05-01T00:00:00Z', '2030-05-19T00:00:00Z')
     expect(isValid).toBe(true)
   })
 })
