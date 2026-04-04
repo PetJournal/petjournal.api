5bbeefda16cc38e6843c79c0c95b062defa50d29
andersonnaz <anderson_1004@hotmail.com>
2026-04-03 22:41:31 -0300

    feat: change validators to permit numbers

--- src/infra/validators/title-validator-adapter.ts
+++ src/infra/validators/title-validator-adapter.ts
@@ -3,7 +3,7 @@ import validator from 'validator'
 
 export class TitleValidatorAdapter implements TitleValidator {
   isValid (title: string): boolean {
-    const isValidTitle = (field: string): boolean => validator.matches(field, /^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]{3,}$/)
+    const isValidTitle = (field: string): boolean => validator.matches(field, /^[a-zA-Z0-9áàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ .,-]{3,}$/)
 
     if (isValidTitle(title)) {
       return true
