24498abe2aae8a20d93d02fc1b5553d22a639007
andersonnaz <anderson_1004@hotmail.com>
2026-04-13 16:09:47 -0300

    refactor: change route name and imports

--- src/main/routes/load-guardian-routes.ts
+++ src/main/routes/load-guardian-routes.ts
@@ -1,8 +1,8 @@
 import { type Router } from 'express'
 import { accountConfirmation, auth } from '@/main/middlewares'
-import { makeLoadGuardianNameController } from '@/main/factories/controllers'
+import { makeLoadGuardianController } from '@/main/factories/controllers'
 import { adaptRoute } from '@/main/adapters'
 
 export default (router: Router): void => {
-  router.get('/guardian/name', auth, accountConfirmation, adaptRoute(makeLoadGuardianNameController()))
+  router.get('/guardian', auth, accountConfirmation, adaptRoute(makeLoadGuardianController()))
 }
