608e0047c61e8cb56ec0d8c9a4e01cfd65447d33
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2026-05-29 10:36:09 -0300

    test: atualiza teste

--- tests/helpers/integration-setup.ts
+++ tests/helpers/integration-setup.ts
@@ -1,9 +1,10 @@
-import { FirebaseStorageAdapter } from '@/infra/repos/firebase'
-import { MailerooAdapter } from '@/infra/communication'
-
+/* eslint-disable @typescript-eslint/no-var-requires */
 const testPath = expect.getState().testPath
 
 if (testPath && testPath.endsWith('.test.ts')) {
+  const { FirebaseStorageAdapter } = require('@/infra/repos/firebase')
+  const { MailerooAdapter } = require('@/infra/communication')
+
   jest.spyOn(FirebaseStorageAdapter.prototype, 'save').mockResolvedValue('https://storage.googleapis.com/mock-bucket/images/mock-file.jpg')
   jest.spyOn(FirebaseStorageAdapter.prototype, 'delete').mockResolvedValue(undefined)
   jest.spyOn(MailerooAdapter.prototype, 'send').mockResolvedValue(true)
